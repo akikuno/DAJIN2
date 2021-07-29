53c5cea509544f8ef5b0140904e8513480cd6e08
Akihiro Kuno <akikuno@users.noreply.github.com>
2021-07-29 10:56:08 +0900

    Update test_midsToscore.sh

--- test/midsToscore/test_midsToscore.sh
+++ test/midsToscore/test_midsToscore.sh
@@ -19,64 +19,17 @@ aaa,M,2M,M,D,M
 bbb,M,2M,D,D,M
 ccc,M,1M,M,M,S
 EOF
-  expansion >tmp.csv
+  midsToscore
 
-# cat <<EOF |
-# aaa,M,2M,M,D,M,M,M,M,M
-# bbb,M,2M,D,D,M,D,D,D,M
-# ccc,M,1M,M,D,S,S,M,M,M
-# EOF
-#   expansion >tmp.csv
+cat <<EOF |
+aaa,M,2M,M,D,M,M,M,M,M
+bbb,M,2M,D,D,M,D,D,D,M
+ccc,M,1M,M,D,S,S,M,M,M
+EOF
+  midsToscore
 
-# cat <<EOF |
-# aaa,D,D,M,D,D,M
-# bbb,D,D,M,D,D,M
-# EOF
-#   expansion >tmp.csv
-
-cat tmp.csv |
-  awk -f library/mutToat.awk |
-  awk -f library/atTonum.awk >tmp_row.csv
-
-cat tmp.csv |
-  Rscript library/colTable.R >tmp_col.csv
-
-cat tmp.csv |
-  awk '{
-    FS=","
-    OFS=""
-    $1=$1","
-    for(i=2;i<=NF;i++) {
-      if($i>0 && $(i+1)>0)
-        $i="@"
-      else if($(i-1)=="@" && $i==1)
-        $i="@,"
-      else
-        $i=$i","
-    }
-    sub(",$", "", $NF)
-  }1'
-
-rowSums tmp
-
-cat tmp |
-  awk -F, '{
-    id[NR]=$1
-    for(i=2; i<=NF; i++) {
-      sums[i]+=$i
-      if($i>0) {
-        array[i, $i]++
-      } else {
-        array[i, $i]=0
-      }
-    }
-  } END {
-    for(nr=1; nr<=NR; nr++) {
-      printf id[nr]","
-      for(i=2; i<=NF; i++) {
-        for(sum=1; sum<=sums[i]; sum++) {
-          printf array[i, sum]
-        }
-        }
-      }
-    }'
+cat <<EOF |
+aaa,D,D,M,D,D,M
+bbb,D,D,M,D,D,M
+EOF
+  midsToscore
