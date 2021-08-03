e805d3da8073034c6f7fa324fee55204189a12b4
Akihiro Kuno <akikuno@users.noreply.github.com>
2021-08-03 12:18:32 +0900

    refactoring

--- script/02-args/01-parse.sh
+++ script/02-args/01-parse.sh
@@ -76,7 +76,7 @@ echo "$ARGS" |
 control_name="$(basename "$control" | sed "s/\..*$//" | tr " " "_")"
 sample_name="$(basename "$sample" | sed "s/\..*$//" | tr " " "_")"
 
-[ ${control_name:-} ] || error_exit "$control is an invalid file name."
-[ ${sample_name:-} ] || error_exit "$sample is an invalid file name"
+[ "${control_name:-}" ] || error_exit "$control is an invalid file name."
+[ "${sample_name:-}" ] || error_exit "$sample is an invalid file name"
 
 # Check optional arguments ------------
