7ea736464207a8166b1da336d339d171754e459c
Akihiro Kuno <akikuno@users.noreply.github.com>
2021-08-07 21:36:30 +0900

    update filtering

--- script/03-preprocess/035-mids-filter.sh
+++ script/03-preprocess/035-mids-filter.sh
@@ -4,6 +4,17 @@ mkdir -p .DAJIN_temp/midsfilter/
 
 find .DAJIN_temp/mids/*.csv |
   while read -r line; do
-    awk -F, '$51 != "D" && $(NF-50) != "D"' "$line" |
-      cat >.DAJIN_temp/midsfilter/"$(basename "$line")"
+    cat "$line" |
+      awk -F, '{
+      leftD=0
+      rightD=0
+      for (i=2; i<=51; i++) {
+        if ($i=="D") leftD++
+      }
+      for (i=NF-49; i<=NF; i++) {
+        if ($i=="D") rightD++
+      }
+      if ( leftD!=50 || rightD!=50)
+        print $0
+      }' >.DAJIN_temp/midsfilter/"$(basename "$line")"
   done
