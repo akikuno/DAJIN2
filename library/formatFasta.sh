bb9c936de218cba39a10116e7cdc17f7becee924
Akihiro Kuno <akikuno@users.noreply.github.com>
2021-08-11 15:47:14 +0900

    Update format fasta: paste - -

--- library/formatFasta.sh
+++ library/formatFasta.sh
@@ -3,10 +3,6 @@
 formatFasta() {
   cat "$1" |
     tr -d "\r" |
-    awk 'BEGIN {RS = ">"} {
-      $1=">" $1 " "
-      for(i=1;i<=NF;i++) printf $i
-      print ""
-      }' |
+    paste - - |
     awk 'NF==2 {$2=toupper($2)}1'
 }
