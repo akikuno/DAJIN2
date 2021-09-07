8c84549a7130f29d8e8b362a75d21ed9b7b6329f
Akihiro Kuno <akikuno@users.noreply.github.com>
2021-09-07 16:54:23 +0900

    prepare python packaging

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="DAJIN2",
+    version='2.0',
+    description='One-step genotyping tools for Nanopore amplicon sequencing',
+    author='Akihiro Kuno',
+    author_email='akuno@md.tsukuba.ac.jp',
+    url='https://github.com/akikuno/DAJIN2',
+)
