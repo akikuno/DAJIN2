640a396d7c06642d73b35b4b85cae03e9ffc8a4f
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-03-28 15:37:49 +0900

    add mappy test

--- tests/test_mappy.py
+++ tests/test_mappy.py
@@ -1,20 +1,24 @@
-import mappy as mp
-import cstag
 import re
+import cstag
+import mappy as mp
 
 reffa = "tests/data/mappy/ref.fa"
 quefq = "tests/data/mappy/query.fq"
 
+
 refname, refseq, _ = list(mp.fastx_read(reffa))[0]
-ref = mp.Aligner(reffa)
-
-que_map = []
-for name, seq, qual in mp.fastx_read(quefq):
-    for hit in ref.map(seq, cs=True):
-        que_map.append([hit.r_st, hit.cigar_str, hit.cs])
 
 
-def generate_query_seq(CSTAG, REFSEQ):
+def extract_mappy_tag(REFFA, QUEFQ):
+    ref = mp.Aligner(REFFA)
+    qname = [qname for qname, _, _ in mp.fastx_read(QUEFQ)]
+    for i, (_, seq, _) in enumerate(mp.fastx_read(QUEFQ)):
+        for hit in ref.map(seq, cs=True):
+            yield qname[i], hit.r_st, hit.cigar_str, hit.cs
+
+
+def generate_query_seq(CSTAG, REFSEQ, R_ST):
+    refseq = REFSEQ[R_ST:]
     cs = re.split(r"([-:+*])", CSTAG)[1:]
     cs = [i + j for i, j in zip(cs[0::2], cs[1::2])]
     query_seq = []
@@ -22,7 +26,7 @@ def generate_query_seq(CSTAG, REFSEQ):
     for c in cs:
         if c[0] == ":":
             end = start + int(c[1:])
-            query_seq.append(REFSEQ[start:end])
+            query_seq.append(refseq[start:end])
             start = end
         elif c[0] == "+":
             query_seq.append(c[1:])
@@ -35,10 +39,11 @@ def generate_query_seq(CSTAG, REFSEQ):
 
 
 cslengthen = []
-for r_st, cigar, cs in que_map:
-    r_seq = refseq[r_st:]
-    queseq = generate_query_seq(cs, r_seq)
+qname_cslengthen = []
+for qname, r_st, cigar, cs in extract_mappy_tag(reffa, quefq):
+    queseq = generate_query_seq(cs, refseq, r_st)
     cslengthen.append(cstag.lengthen(cs, cigar, queseq))
+    qname_cslengthen.append([qname, cstag.lengthen(cs, cigar, queseq)])
 
 
 ###############################################################################
@@ -50,11 +55,17 @@ with open(sam) as f:
     sam = f.read().splitlines()
 
 cslong = [s.split("\t")[-2] for s in sam if "cs:Z" in s]
+qname = [q.split("\t")[0] for q in sam if "cs:Z" in q]
 
 
 def test_cslengthen():
     for cs, cs_ans in zip(cslengthen, cslong):
         assert cs == cs_ans
+
+
+def test_qname_cslengthen():
+    for cs, cs_ans in zip(qname_cslengthen, zip(qname, cslong)):
+        assert tuple(cs) == cs_ans
 
 
 # END
