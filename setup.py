1f382a47f7dbd19f7cbed1c17e73c7af25353a7f
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-03-29 13:51:43 +0900

    add dependencies

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup
 
 setup(
     name="DAJIN2",
-    version='2.0',
-    description='One-step genotyping tools for Nanopore amplicon sequencing',
-    author='Akihiro Kuno',
-    author_email='akuno@md.tsukuba.ac.jp',
-    url='https://github.com/akikuno/DAJIN2',
+    version="1.0.0",
+    description="One-step genotyping tools for Nanopore amplicon sequencing",
+    author="Akihiro Kuno",
+    author_email="akuno@md.tsukuba.ac.jp",
+    url="https://github.com/akikuno/DAJIN2",
+    install_requires=["numpy", "pandas", "scikit-learn", "hdbscan", "plotnine", "mappy", "pysam"],
 )
