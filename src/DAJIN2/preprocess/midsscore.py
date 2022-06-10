86e8eef431ff083e187e7219b2e412c4d4c3373a
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-06-10 17:43:37 +0900

    work in progress...

--- src/DAJIN2/preprocess/midsscore.py
+++ src/DAJIN2/preprocess/midsscore.py
@@ -19,18 +19,45 @@ output: 各リードごとに一番ミスマッチが少なかったアレル
 00001,inversion,"M,4M,D,M" -> 2/
 """
 
+import re
 import pathlib
-
-
-def append_name(sample_name: str) -> dict:
-    filePath = pathlib.Path(".tmpDAJIN", "midsconv").glob(f"{sample_name}*")
-    for f in filePath:
-        allele_name = f.name.replace(f"{sample_name}_", "").replace(".csv", "")
-        tmp = f.read_text().split()
-        scores = [calc(tmp)
+from itertools import groupby
 
 
 def calc(mids: str) -> float:
     mids_list = mids.split(",")[1:]
-    return mids_list.count("M") / len(mids_list)
+    mids_length = len(mids_list)
+    for ins in mids_list:
+        if ins[0].isdigit():
+            mids_length += int(re.sub(r"M|D|S", "", ins))
+    return mids_list.count("M") / mids_length
 
+
+readid_allele_score = []
+
+# def append_name(sample_name: str) -> dict:
+
+midspath = pathlib.Path(".tmpDAJIN", "midsconv").glob(f"{sample_name}*")
+for mids in midspath:
+    allele_name = mids.stem.replace(f"{sample_name}_", "")
+    for midscsv in mids.read_text().split():
+        readid = midscsv.split(",")[0]
+        score = calc(midscsv)
+        readid_allele_score.append({"readid": readid, "allele": allele_name, "score": score})
+
+readid_allele_score.sort(key=lambda x: x["readid"])
+
+readid_allele_score_extract = []
+for key, group in groupby(readid_allele_score, key=lambda x: x["readid"]):
+    max_score = 0
+    for member in group:
+        if member["score"] > max_score:
+            tmp_read = member
+            max_score = member["score"]
+    readid_allele_score_extract.append(tmp_read)
+
+from collections import defaultdict
+
+d = defaultdict(int)
+for x in readid_allele_score_extract:
+    d[x["allele"]] += 1
