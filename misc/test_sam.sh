ea2f68f6f98da0e7e7a3d2a761be79a525b0bd3e
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-07-22 05:42:15 +0900

    update tests

--- misc/test_sam.sh
+++ misc/test_sam.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-sampath=".tmpDAJIN/sam/barcode31_albino.sam"
+sampath=".tmpDAJIN/sam/barcode30_target.sam"
+extract_readid="e657b9f9-ffab-441c-96d8-2a15fbb95ca4"
 
 cat $sampath |
-    # grep -e "^@" -e bdf684fb-2ae6-4a0e-9fee-73d30778c457 |
-    samtools sort >tmp.bam
+    grep -e "^@" -e "$extract_readid" |
+    samtools sort >tmp2.bam
 
-samtools index tmp.bam
+samtools index tmp2.bam
--- tests/test_mappy_align.py
+++ tests/test_mappy_align.py
@@ -1,6 +1,6 @@
+from importlib import reload
 from pathlib import Path
 from src.DAJIN2.preprocess import mappy_align
-from importlib import reload
 
 reload(mappy_align)
 
@@ -14,29 +14,29 @@ def test_to_sam():
     assert value == answer
 
 
-def test_remove_unmapped_reads():
-    sam = Path("tests", "data", "mappy", "unmapped.sam").read_text().strip().split("\n")
-    value = mappy_align.remove_unmapped_reads(sam)
-    value = list(value)
-    answer = Path("tests", "data", "mappy", "unmapped_removed.sam").read_text().strip().split("\n")
-    assert value == answer
+# def test_remove_unmapped_reads():
+#     sam = Path("tests", "data", "mappy", "unmapped.sam").read_text().strip().split("\n")
+#     value = mappy_align.remove_unmapped_reads(sam)
+#     value = list(value)
+#     answer = Path("tests", "data", "mappy", "unmapped_removed.sam").read_text().strip().split("\n")
+#     assert value == answer
 
 
-def test_remove_overlaped_reads():
-    sam = Path("tests", "data", "mappy", "overlapped.sam").read_text().strip().split("\n")
-    value = mappy_align.remove_overlapped_reads(sam)
-    value = list(value)
-    answer = Path("tests", "data", "mappy", "overlapped_removed.sam").read_text().strip().split("\n")
-    assert value == answer
+# def test_remove_overlaped_reads():
+#     sam = Path("tests", "data", "mappy", "overlapped.sam").read_text().strip().split("\n")
+#     value = mappy_align.remove_overlapped_reads(sam)
+#     value = list(value)
+#     answer = Path("tests", "data", "mappy", "overlapped_removed.sam").read_text().strip().split("\n")
+#     assert value == answer
 
 
-def test_remove_overlaped_reads_2():
-    """Input is truely inversion so the function must return the same of the input.
-    """
-    sam = Path("tests", "data", "mappy", "inversion.sam").read_text().strip().split("\n")
-    value = mappy_align.remove_overlapped_reads(sam)
-    value = list(value)
-    assert value == sam
+# def test_remove_overlaped_reads_2():
+#     """Input is truely inversion so the function must return the same of the input.
+#     """
+#     sam = Path("tests", "data", "mappy", "inversion.sam").read_text().strip().split("\n")
+#     value = mappy_align.remove_overlapped_reads(sam)
+#     value = list(value)
+#     assert value == sam
 
 
 # Create test data
