35783fc391010ed13f27349b0fcf097aa2f346dd
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-08-01 11:16:58 +0900

    move mdsvscore module to classification

---
+++ src/DAJIN2/classification/midsvscore.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+import midsv
+from pathlib import Path
+from itertools import groupby
+
+
+def calc_match(CSSPLIT: str) -> float:
+    match_score = CSSPLIT.count("=")
+    match_score -= CSSPLIT.count("N")  # unknown
+    match_score -= CSSPLIT.count("+")  # insertion
+    match_score -= sum(cs.islower() for cs in CSSPLIT)  # inversion
+    return match_score / len(CSSPLIT.split(","))
+
+
+def classify_alleles(sample_name: str) -> list[dict]:
+    score_of_each_allels = []
+    midsvpaths = Path(".tmpDAJIN", "midsv").glob(f"{sample_name}*")
+    for midsvpath in midsvpaths:
+        ALLELE = midsvpath.stem.replace(f"{sample_name}_", "")
+        for midsvcsv in midsv.read_jsonl(midsvpath):
+            QNAME = midsvcsv["QNAME"]
+            CSSPLIT = midsvcsv["CSSPLIT"]
+            SCORE = calc_match(CSSPLIT)
+            score_of_each_allels.append({"QNAME": QNAME, "ALLELE": ALLELE, "SCORE": SCORE, "CSSPLIT": CSSPLIT})
+    score_of_each_allels.sort(key=lambda x: x["QNAME"])
+    possible_allele = []
+    for _, group in groupby(score_of_each_allels, key=lambda x: x["QNAME"]):
+        max_score = 0
+        for readinfo in group:
+            if readinfo["SCORE"] > max_score:
+                max_score = readinfo["SCORE"]
+                max_read = readinfo
+                del max_read["SCORE"]
+        possible_allele.append(max_read)
+    return possible_allele
