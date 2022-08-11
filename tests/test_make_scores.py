21c700a9d1a64d109f825202a95733d441518226
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-08-11 11:40:35 +0900

    add tests to `make_scores`

---
+++ tests/test_make_scores.py
@@ -0,0 +1,74 @@
+from src.DAJIN2 import clustering
+from importlib import reload
+
+reload(clustering)
+
+
+def test_repeat_del():
+    sample_cssplit = ["-A,-A,=A", "=A,-A,=A", "-A,-A,-A"]
+    diff_locus = [0, 1, 2]
+    test = clustering.make_scores(sample_cssplit, diff_locus)
+    test = list(test)
+    answer = [
+        [[0, 0, 0, 0, 2, 0, 0], [0, 0, 0, 0, 2, 0, 0], [0, 0, 0, 0, 0, 0, 0]],
+        [[0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 0, 0]],
+        [[0, 0, 0, 0, 3, 0, 0], [0, 0, 0, 0, 3, 0, 0], [0, 0, 0, 0, 3, 0, 0]],
+    ]
+    assert test == answer
+
+
+def test_repeat_substitution():
+    sample_cssplit = ["*AC,*AC,=A", "=A,*AC,=A", "*AC,*AC,*AC"]
+    diff_locus = [0, 1, 2]
+    test = clustering.make_scores(sample_cssplit, diff_locus)
+    test = list(test)
+    answer = [
+        [[0, 0, 0, 0, 0, 2, 0], [0, 0, 0, 0, 0, 2, 0], [0, 0, 0, 0, 0, 0, 0]],
+        [[0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1, 0], [0, 0, 0, 0, 0, 0, 0]],
+        [[0, 0, 0, 0, 0, 3, 0], [0, 0, 0, 0, 0, 3, 0], [0, 0, 0, 0, 0, 3, 0]],
+    ]
+    assert test == answer
+
+
+def test_repeat_N():
+    sample_cssplit = ["N,N,=A", "=A,N,=A", "N,N,N"]
+    diff_locus = [0, 1, 2]
+    test = clustering.make_scores(sample_cssplit, diff_locus)
+    test = list(test)
+    answer = [
+        [[0, 0, 0, 0, 0, 0, 2], [0, 0, 0, 0, 0, 0, 2], [0, 0, 0, 0, 0, 0, 0]],
+        [[0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 1], [0, 0, 0, 0, 0, 0, 0]],
+        [[0, 0, 0, 0, 0, 0, 3], [0, 0, 0, 0, 0, 0, 3], [0, 0, 0, 0, 0, 0, 3]],
+    ]
+    assert test == answer
+
+
+def test_inversion():
+    sample_cssplit = ["-a,*AC,-a", "=A,+c|c|c|=a,=A", "=A,+c|c|c|*ac,=A", "=A,*ac,=A", "=A,*ac,-a"]
+    diff_locus = [0, 1, 2]
+    test = clustering.make_scores(sample_cssplit, diff_locus)
+    test = list(test)
+    answer = [
+        [[0, 0, 0, 0, -1, 0, 0], [0, 0, 0, 0, 0, 1, 0], [0, 0, 0, 0, -1, 0, 0]],
+        [[0, 0, 0, 0, 0, 0, 0], [-4, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]],
+        [[0, 0, 0, 0, 0, 0, 0], [0, 0, -4, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]],
+        [[0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, -1, 0], [0, 0, 0, 0, 0, 0, 0]],
+        [[0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, -1, 0], [0, 0, 0, 0, -1, 0, 0]],
+    ]
+    assert test == answer
+
+
+def test_insertion():
+    sample_cssplit = ["=A,+C|C|C|=A,=A", "=A,+C|C|C|-A,=A", "=A,+C|C|C|*CG,=A", "=A,+C|C|C|N,=A", "=A,+c|c|c|n,=A"]
+    diff_locus = [0, 1, 2]
+    test = clustering.make_scores(sample_cssplit, diff_locus)
+    test = list(test)
+    answer = [
+        [[0, 0, 0, 0, 0, 0, 0], [4, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]],
+        [[0, 0, 0, 0, 0, 0, 0], [0, 4, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]],
+        [[0, 0, 0, 0, 0, 0, 0], [0, 0, 4, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]],
+        [[0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 4, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]],
+        [[0, 0, 0, 0, 0, 0, 0], [0, 0, 0, -4, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]],
+    ]
+    assert test == answer
+
