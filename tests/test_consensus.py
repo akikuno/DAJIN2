95e377578fa88a3415a35f1dc131e6a234c60daa
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-09-07 05:56:14 +0900

    remove unused tests

--- tests/test_consensus.py
+++ tests/test_consensus.py
@@ -4,9 +4,9 @@ from importlib import reload
 reload(module_consensus)
 
 
-def test_module_consensus():
-    cssplits = ["=A,*GA,=C", "=A,*GA,-C", "=A,*GA,-C"]
-    test = module_consensus.call(cssplits)
-    answer = "=A,*GA,-C"
-    assert test == answer
+# def test_module_consensus():
+#     cssplits = ["=A,*GA,=C", "=A,*GA,-C", "=A,*GA,-C"]
+#     test = module_consensus.call(cssplits)
+#     answer = "=A,*GA,-C"
+#     assert test == answer
 
