009f8881f17917626ba548001c13f82f389418b8
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-09-23 13:55:08 +0900

    add PyPI testing

--- misc/local-pytest.sh
+++ misc/local-pytest.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-mamba create -n test-DAJIN2 pytest --yes
-mamba install -n test-DAJIN2 --file requirements.txt --yes
-conda activate test-DAJIN2
-if pytest; then
+mamba create -n test-conda-DAJIN2 pytest --yes
+conda activate test-conda-DAJIN2
+mamba install -n test-conda-DAJIN2 --file requirements.txt --yes || flag=1
+python -m pytest -p no:warnings || flag=1
+if [ "${flag:-0}" -eq 0 ]; then
     conda activate base
-    mamba remove -n test-DAJIN2 --all
+    mamba remove -n test-conda-DAJIN2 --all
+fi
+
+mamba create -n test-pypi-DAJIN2
+conda activate test-pypi-DAJIN2
+python -m pip install --upgrade pip
+python -m pip install pytest
+python -m pip install -r requirements.txt || flag=1
+python -m pytest -p no:warnings || flag=1
+if [ "${flag:-0}" -eq 0 ]; then
+    conda activate base
+    mamba remove -n test-pypi-DAJIN2 --all
 fi
