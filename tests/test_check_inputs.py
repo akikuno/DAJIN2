4e6490748adb399a4b65232d8c1f08cbcb3b8823
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-09-27 08:42:08 +0900

    add tests to `check_inputs`

--- tests/test_check_inputs.py
+++ tests/test_check_inputs.py
@@ -5,26 +5,34 @@ from importlib import reload
 reload(check_inputs)
 
 
+###############################################################################
+# Check File existance
+###############################################################################
+
+
+def test_exists():
+    with pytest.raises(FileNotFoundError) as e:
+        test = "filenotfound.txt"
+        check_inputs.exists(test)
+    assert str(e.value) == f"{test} is not found"
+
+
+###############################################################################
+# Check FASTQ
+###############################################################################
+
+
 def test_fastq_extension():
-    with pytest.raises(AttributeError) as excinfo:
+    with pytest.raises(AttributeError) as e:
+        test = "test.fqq"
         check_inputs.fastq_extension("test.fqq")
-    assert str(excinfo.value) == "test.fqq requires extensions either 'fastq', 'fastq.gz', 'fq' or 'fq.gz'"
+    assert str(e.value) == f"{test} requires extensions either 'fastq', 'fastq.gz', 'fq' or 'fq.gz'"
 
 
-# Contents check
-
-fastq_path = "tests/data/empty.txt"
-
-
-def test_fastq_error():
+def test_fastq_error_not_fastq_format():
     with pytest.raises(AttributeError):
+        fastq_path = "tests/data/check_inputs/empty.fq"
         _ = check_inputs.fastq_content(fastq_path)
-
-
-def test_fasta_error():
-    with pytest.raises(AttributeError):
-        fasta_path = "tests/data/empty.txt"
-        _ = check_inputs.fasta_content(fasta_path)
 
 
 def test_fastq_without_error():
@@ -32,9 +40,47 @@ def test_fastq_without_error():
     assert check_inputs.fastq_content(fasta_path) is None
 
 
+###############################################################################
+# Check FASTA
+###############################################################################
+
+
+def test_fasta_error_not_fasta_format():
+    with pytest.raises(AttributeError) as e:
+        fasta_path = "tests/data/check_inputs/empty.fa"
+        _ = check_inputs.fasta_content(fasta_path)
+    assert str(e.value) == f"{fasta_path} is not a FASTA format"
+
+
+def test_fasta_error_duplicated_identifiers():
+    with pytest.raises(AttributeError) as e:
+        fasta_path = "tests/data/check_inputs/duplicated_name.fa"
+        _ = check_inputs.fasta_content(fasta_path)
+    assert str(e.value) == f"{fasta_path} must include unique identifiers"
+
+
+def test_fasta_error_duplicated_sequences():
+    with pytest.raises(AttributeError) as e:
+        fasta_path = "tests/data/check_inputs/duplicated_seq.fa"
+        _ = check_inputs.fasta_content(fasta_path)
+    assert str(e.value) == f"{fasta_path} must include unique DNA sequences"
+
+
+def test_fasta_error_without_control():
+    with pytest.raises(AttributeError) as e:
+        fasta_path = "tests/data/check_inputs/no_control.fa"
+        _ = check_inputs.fasta_content(fasta_path)
+    assert str(e.value) == f"{fasta_path} must include a 'control' sequence"
+
+
 def test_fasta_without_error():
     fasta_path = "examples/nanosim/del-stx2/design_stx2.fa"
     assert check_inputs.fasta_content(fasta_path) is None
+
+
+###############################################################################
+# Check URL
+###############################################################################
 
 
 def test_available_url_pass():
@@ -49,13 +95,16 @@ def test_available_url_fail():
 
 def test_available_genome_pass():
     genome = "mm10"
-    url = f"https://genome.ucsc.edu/cgi-bin/das/{genome}/dna?segment=1:1,10"
-    assert check_inputs.available_genome("mm10", url) is None
+    ucsc_url = "https://genome.ucsc.edu/"
+    assert check_inputs.available_genome(genome, ucsc_url) is None
 
 
 def test_available_genome_fail():
     genome = "xxxx"
-    url = f"https://genome.ucsc.edu/cgi-bin/das/{genome}/dna?segment=1:1,10"
-    with pytest.raises(AttributeError) as excinfo:
-        check_inputs.available_genome(genome, url)
-    assert str(excinfo.value) == f"{genome} is not listed in UCSC genome browser"
+    ucsc_url = "https://genome.ucsc.edu/"
+    with pytest.raises(AttributeError) as e:
+        check_inputs.available_genome(genome, ucsc_url)
+    assert (
+        str(e.value)
+        == f"{genome} is not listed in UCSC genome browser. Available genomes are in {ucsc_url}/cgi-bin/das/dsn"
+    )
