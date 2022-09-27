771f87d30a50ed94e032c9f01e00f2c3f8e16749
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-09-27 16:49:02 +0900

    bug fix when n_clusters > 3

--- src/DAJIN2/clustering/module_clustering.py
+++ src/DAJIN2/clustering/module_clustering.py
@@ -11,24 +11,18 @@ def return_labels(xflatten, n_clusters=1):
     return brc.predict(xflatten)
 
 
-def make_table(labels, n_clusters=1):
+def make_table(labels):
     count = defaultdict(int)
     for label in labels:
         count[label] += 1
     table = list(count.values())
     table.sort(reverse=True)
-    if n_clusters == 1:
-        table += [0]
-    elif n_clusters == 2:
-        pass
-    else:
-        table = table[:-1]
     return table
 
 
-def chistatistic(s_table, c_table, threshold=0.05) -> float:
-    chisq_value, _, ddof, _ = st.chi2_contingency([s_table, c_table])
-    delta = sum(s_table + c_table) * threshold ** 2
+def chistatistic(prev_table, current_table, threshold=0.05) -> float:
+    chisq_value, _, ddof, _ = st.chi2_contingency([prev_table, current_table])
+    delta = sum(prev_table + current_table) * threshold ** 2
     pval = 1 - st.ncx2.cdf(chisq_value, ddof, delta)
     return pval
 
@@ -38,20 +32,20 @@ def clustering(scores):
     xarr = np.array(scores)
     xflatten = np.reshape(xarr, (len(xarr), -1))
     xflatten = csr_matrix(xflatten)
-    # Clustering
-    prev_labels = [0 for _ in range(len(scores))]
-    prev_labels = np.array(prev_labels)
-    prev_table = make_table(prev_labels, n_clusters=1)
-    n_clusters = 2
+    n_clusters = 1
     while True:
-        current_labels = return_labels(xflatten, n_clusters=n_clusters)
-        current_table = make_table(current_labels, n_clusters=n_clusters)
+        prev_labels = return_labels(xflatten, n_clusters=n_clusters)
+        prev_table = make_table(prev_labels)
+        current_labels = return_labels(xflatten, n_clusters=n_clusters + 1)
+        current_table = make_table(current_labels)
+        if n_clusters == 1:
+            prev_table += [0]
+        else:
+            current_table = current_table[:-1]
         pval = chistatistic(prev_table, current_table, threshold=0.05)
         if pval > 0.05:
             labels = prev_labels
             break
-        prev_table = current_table
-        prev_labels = current_labels
         n_clusters += 1
     return labels
 
