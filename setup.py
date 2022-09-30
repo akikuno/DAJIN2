f43a507942a914d9bf93ade8897795d648ed128d
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-09-30 12:52:18 +0900

    add command path

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as requirements_file:
+    install_requirements = requirements_file.read().splitlines()
+
 from setuptools import setup
 
 setup(
@@ -7,4 +12,9 @@ setup(
     author="Akihiro Kuno",
     author_email="akuno@md.tsukuba.ac.jp",
     url="https://github.com/akikuno/DAJIN2",
+    packages=find_packages(),
+    install_requires=install_requirements,
+    entry_points={"console_scripts": ["DAJIN2=src.DAJIN2.DAJIN2:main"]},
+    # package_dir={"DAJIN2": "src/DAJIN2",},
+    # package_data={"DAJIN2": ["core/*.py",]},
 )
