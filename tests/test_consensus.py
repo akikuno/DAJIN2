bf932f45af286bc2adbd33a6fb1d5f9eae06d1df
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-10-09 06:12:36 +0900

    fix module name

--- tests/test_consensus.py
+++ tests/test_consensus.py
@@ -6,56 +6,56 @@ reload(consensus)
 
 def test_call_sequence_match():
     cons_percentage_by_key = [{"=A": 1.0}, {"=T": 0.9, "-T": 0.1}]
-    test = consensus.call.call_sequence(cons_percentage_by_key)
+    test = consensus.call_sequence(cons_percentage_by_key)
     answer = "AT"
     assert test == answer
 
 
 def test_call_sequence_deletion():
     cons_percentage_by_key = [{"=A": 1.0}, {"-A": 0.9, "=A": 0.1}, {"=T": 1.0}]
-    test = consensus.call.call_sequence(cons_percentage_by_key)
+    test = consensus.call_sequence(cons_percentage_by_key)
     answer = "AT"
     assert test == answer
 
 
 def test_call_sequence_substitution():
     cons_percentage_by_key = [{"=A": 1.0}, {"*AC": 0.9, "-A": 0.1}, {"=T": 1.0}]
-    test = consensus.call.call_sequence(cons_percentage_by_key)
+    test = consensus.call_sequence(cons_percentage_by_key)
     answer = "ACT"
     assert test == answer
 
 
 def test_call_sequence_inversion():
     cons_percentage_by_key = [{"=A": 1.0}, {"=a": 0.9, "-a": 0.1}, {"=T": 1.0}]
-    test = consensus.call.call_sequence(cons_percentage_by_key)
+    test = consensus.call_sequence(cons_percentage_by_key)
     answer = "AaT"
     assert test == answer
 
 
 def test_call_sequence_insertion_match():
     cons_percentage_by_key = [{"=A": 1.0}, {"+G|+G|+G|=A": 0.9, "-A": 0.1}, {"=T": 1.0}]
-    test = consensus.call.call_sequence(cons_percentage_by_key)
+    test = consensus.call_sequence(cons_percentage_by_key)
     answer = "AGGGAT"
     assert test == answer
 
 
 def test_call_sequence_insertion_deletion():
     cons_percentage_by_key = [{"=A": 1.0}, {"+G|+G|+G|-A": 0.9, "-A": 0.1}, {"=T": 1.0}]
-    test = consensus.call.call_sequence(cons_percentage_by_key)
+    test = consensus.call_sequence(cons_percentage_by_key)
     answer = "AGGGT"
     assert test == answer
 
 
 def test_call_sequence_insertion_substitution():
     cons_percentage_by_key = [{"=A": 1.0}, {"+G|+G|+G|*AT": 0.9, "-A": 0.1}, {"=T": 1.0}]
-    test = consensus.call.call_sequence(cons_percentage_by_key)
+    test = consensus.call_sequence(cons_percentage_by_key)
     answer = "AGGGTT"
     assert test == answer
 
 
 def test_call_sequence_insertion_N():
     cons_percentage_by_key = [{"=A": 1.0}, {"+G|+G|+G|N": 0.9, "-A": 0.1}, {"=T": 1.0}]
-    test = consensus.call.call_sequence(cons_percentage_by_key)
+    test = consensus.call_sequence(cons_percentage_by_key)
     answer = "AGGGNT"
     assert test == answer
 
