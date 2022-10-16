805b6e9696024c38ce48bdfac6e54be9f80afb48
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-10-17 06:33:11 +0900

    change to `plotly` from `plotnine`

--- src/DAJIN2/batch.py
+++ src/DAJIN2/batch.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
 from pathlib import Path
 from itertools import groupby
+import shutil
+import pandas as pd
 from .core import main
 import wslPath
 import midsv
-import pandas as pd
+from src.DAJIN2.batchmode import report
+from jinja2 import Template, Environment, FileSystemLoader
 
 
 def batch(arguments: dict[str]):
@@ -58,29 +61,67 @@ def batch(arguments: dict[str]):
     contents.sort(key=lambda x: x[index_name])
 
     for name, groups in groupby(contents, key=lambda x: x[index_name]):
-        output_dir = Path("DAJINResults", ".tempdir", name, "batch")
-        output_dir.mkdir(exist_ok=True, parents=True)
+        report_directory = Path("DAJINResults", name)
+        report_directory.mkdir(exist_ok=True, parents=True)
+        batch_directory = Path("DAJINResults", ".tempdir", name, "batch")
+        batch_directory.mkdir(exist_ok=True, parents=True)
         for group in groups:
             args = {h: g for h, g in zip(keys, group)}
             args["threads"] = threads
+            # perform DAJIN
             SAMPLE_NAME, RESULT = main.main(args)
             # output the result
-            midsv.write_jsonl(RESULT, Path(output_dir, f"{SAMPLE_NAME}.jsonl"))
-            print(
-                f"batch.py: {SAMPLE_NAME}.jsonl is completed"
-            )  # !----------------------------------------------------------------------------------------------
+            midsv.write_jsonl(RESULT, Path(batch_directory, f"{SAMPLE_NAME}.jsonl"))
+            # ? DEVELOPMENT------------------------------------------------------------------
+            print(f"DEVELOPLENT: batch.py: {SAMPLE_NAME}.jsonl is completed")
+            # ? DEVELOPMENT------------------------------------------------------------------
+        # Reports -----------------------------------------
+        for dir in Path("DAJINResults", ".tempdir", name, "report").iterdir():
+            shutil.copytree(dir, Path(report_directory, dir.stem), dirs_exist_ok=True)
+        df_all = report.all_info(batch_directory)
+        df_all.to_csv(Path(report_directory, "read_all.csv"), index=False)
+        df_summary = report.summary_info(df_all)
+        df_summary.to_csv(Path(report_directory, "read_summary.csv"), index=False)
+        report.output_plot(df_summary, report_directory)
 
 
-# ##############################################################################
-# # Reports
-# ##############################################################################
-# name = "Ayabe-Task1"
-# output_dir = Path("DAJINResults", f".tempdir_{name}", "batch")
-# df_results = pd.DataFrame()
-# for path_result in output_dir.iterdir():
-#     SAMPLE_NAME = path_result.stem
-#     result = midsv.read_jsonl(path_result)
-#     df_result = pd.DataFrame(result)
-#     df_result["SAMPLE"] = SAMPLE_NAME
-#     df_results = pd.concat([df_results, df_result])
+##############################################################################
+# Reports
+##############################################################################
 
+# IGV.js
+
+# df_summary = df_results.drop(columns=["QNAME", "LABEL", "ALLELE", "SV"]).drop_duplicates()
+# env = Environment(loader=FileSystemLoader("./", encoding="utf8"))
+# template = env.get_template("src/DAJIN2/template_igvjs.html")  #!? DEVELOPMENT ==================================
+# # template = env.get_template("template_igvjs.html")
+# params_genome = dict()
+# params_reference = dict()
+# params_tracks = dict()
+# params_genome = {"genome": {"exist": False}}
+# if GENOME:
+#     chrom, start, end, _ = GENOME_COODINATES.values()
+#     params_genome = {"genome": {"exist": True, "genome": GENOME, "locus": f"{chrom}:{start}-{end}"}}
+# else:
+#     params_reference = {"reference": {"urlfa": f""}}
+
+# params_reference = {"reference": {"urlfa": "@@@@@@@@@@@@@@@@@@@"}}
+
+# params_tracks = """{
+#     "tracks":
+#         {{ contents }},
+# }"""
+# params_tracks = Template(params_tracks)
+
+# SAMPLENAMES = ["hoge", "fuga"]
+# URLBAM = ["DAJINResults/.tempdir/Ayabe-Task1/bam/barcode36_allele1_flox_mutated_50.3%.bam", "fuga2"]
+# URLBAI = ["foooo", "foooo2"]
+# contents = [{"samplename": s, "urlbam": b, "urlbai": i} for s, b, i in zip(SAMPLENAMES, URLBAM, URLBAI)]
+
+# params_tracks = eval(params_tracks.render(contents=contents))
+
+# params_genome.update(**params_reference, **params_tracks)
+# params = params_genome.copy()
+
+# result = template.render(params)
+# print(result)
--- src/DAJIN2/batchmode/report.py
+++ src/DAJIN2/batchmode/report.py
@@ -1,13 +1,12 @@
 from __future__ import annotations
 import pandas as pd
-from collections import defaultdict
+import plotly.express as px
 from plotnine import ggplot, aes, geom_bar, theme, theme_bw, element_blank, labs, scale_y_continuous
 import midsv
 from pathlib import Path
 
 
-def all_info(name: str) -> pd.DataFrame:
-    batch_directory = Path("DAJINResults", ".tempdir", name, "batch")
+def all_info(batch_directory: Path) -> pd.DataFrame:
     df_results = pd.DataFrame()
     colum = ["SAMPLE", "QNAME", "NAME", "READNUM", "PERCENT", "LABEL", "ALLELE", "SV"]
     for path_result in batch_directory.iterdir():
@@ -25,18 +24,48 @@ def summary_info(df_all: pd.DataFrame) -> pd.DataFrame:
     return df_summary
 
 
-def plot(df_summary: pd.DataFrame):
+def output_plot(df_summary: pd.DataFrame, report_directory: Path):
     df_plot = df_summary.copy()
     name = df_plot.NAME.tolist()
     names = [n.split("_") for n in name]
     alleletype = [n[1] + " " + n[2] for n in names]
     df_plot["ALLELETYPE"] = alleletype
-    g = (
-        ggplot(df_plot, aes(x="SAMPLE", y="READNUM", fill="ALLELETYPE"))
-        + geom_bar(position="fill", stat="identity", colour="black")
-        + scale_y_continuous(labels=lambda l: ["%d%%" % (v * 100) for v in l])
-        + theme_bw()
-        + theme(axis_title_x=element_blank())
-        + labs(fill="Allele type", y="Percentage of reads")
+    fig = px.bar(
+        df_plot,
+        x="SAMPLE",
+        y="PERCENT",
+        color="ALLELETYPE",
+        text="PERCENT",
+        labels={"SAMPLE": "Samples", "PERCENT": "% of reads", "ALLELETYPE": "Alelle type"},
     )
-    return g
+    fig.update_traces(textposition="inside", cliponaxis=False)
+    output_filename = str(Path(report_directory, "read_plot"))
+    fig.write_html(f"{output_filename}.html")
+    # if kaleido is installed, output a pdf
+    try:
+        fig.write_image(f"{output_filename}.pdf")
+    except ValueError:
+        pass
+
+
+# def plot(df_summary: pd.DataFrame):
+#     df_plot = df_summary.copy()
+#     name = df_plot.NAME.tolist()
+#     names = [n.split("_") for n in name]
+#     alleletype = [n[1] + " " + n[2] for n in names]
+#     df_plot["ALLELETYPE"] = alleletype
+#     g = (
+#         ggplot(df_plot, aes(x="SAMPLE", y="READNUM", fill="ALLELETYPE"))
+#         + geom_bar(position="fill", stat="identity", colour="black")
+#         + scale_y_continuous(labels=lambda l: ["%d%%" % (v * 100) for v in l])
+#         + theme_bw()
+#         + theme(axis_title_x=element_blank())
+#         + labs(fill="Allele type", y="Percentage of reads")
+#     )
+#     return g
+
+
+###############################################################################
+# igvjs
+###############################################################################
+
