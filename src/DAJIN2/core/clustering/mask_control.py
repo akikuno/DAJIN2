0c5824f313aa790016752d4bfadf11e294256f1d
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-10-25 10:23:50 +0900

    debug `clustering.mask_control`

--- src/DAJIN2/core/clustering/mask_control.py
+++ src/DAJIN2/core/clustering/mask_control.py
@@ -1,13 +1,18 @@
-from pathlib import Path
 import midsv
+from pathlib import Path
 from collections import defaultdict
 
 
 def mask_control(TEMPDIR: Path, DICT_ALLELE: dict, CONTROL_NAME: str):
+    """
+    Mask loci that have less than 1% of matches in control seqence.
+    Alignments between control and knock-in alleles produses deletion loci in control.
+    The deletion loci are not sequencing error, so they should be ignored.
+    """
+    masks_control = defaultdict(list)
     for allele in DICT_ALLELE.keys():
         if allele == "control":
             continue
-        masks_control = defaultdict(list)
         path_midsv = Path(TEMPDIR, "midsv", f"{CONTROL_NAME}_{allele}.jsonl")
         cssplit_control = midsv.read_jsonl(path_midsv)
         cssplit_control = [cs["CSSPLIT"].split(",") for cs in cssplit_control]
