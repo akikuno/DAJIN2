3f85d45e68816b17746509ca8b80af9921762d88
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-10-28 09:47:58 +0900

    debug to assign name_fasta

--- src/DAJIN2/core/preprocess/calc_midsv.py
+++ src/DAJIN2/core/preprocess/calc_midsv.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 
 def output_midsv(TEMPDIR, path_sam, DICT_ALLELE):
-    name_fasta = path_sam.stem.split("_")[1]
+    name_fasta = path_sam.stem.split("_")[-1]
     if name_fasta not in set(DICT_ALLELE.keys()):
         return
     sam = midsv.read_sam(path_sam)
