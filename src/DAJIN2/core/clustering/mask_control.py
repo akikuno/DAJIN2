842b5cfae7afd187e48bd3cffd9eca6e521e4f1e
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-11-06 09:16:34 +0900

    change mask threshold from 1% to 20%

--- src/DAJIN2/core/clustering/mask_control.py
+++ src/DAJIN2/core/clustering/mask_control.py
@@ -1,16 +1,17 @@
+from __future__ import annotations
 from collections import defaultdict
 from pathlib import Path
 
 import midsv
 
 
-def mask_control(TEMPDIR: Path, DICT_ALLELE: dict, CONTROL_NAME: str):
+def mask_control(TEMPDIR: Path, DICT_ALLELE: dict, CONTROL_NAME: str) -> defaultdict(set[int]):
     """
-    Mask loci that have less than 1% of matches in control seqence.
+    Mask loci that have less than 20% of matches in control seqence.
     Alignments between control and knock-in alleles produses deletion loci in control.
     The deletion loci are not sequencing error, so they should be ignored.
     """
-    masks_control = defaultdict(list)
+    masks_control = defaultdict(set[int])
     for allele in DICT_ALLELE.keys():
         if allele == "control":
             continue
@@ -19,13 +20,11 @@ def mask_control(TEMPDIR: Path, DICT_ALLELE: dict, CONTROL_NAME: str):
         cssplit_control = [cs["CSSPLIT"].split(",") for cs in cssplit_control]
         coverage = len(cssplit_control)
         cssplit_control_transposed = list(zip(*cssplit_control))
-        masks_loci = []
-        for cs in cssplit_control_transposed:
+        masks_loci = set()
+        for i, cs in enumerate(cssplit_control_transposed):
             count_match = sum(1 for c in cs if c.startswith("="))
             percent_match = count_match / coverage * 100
-            if percent_match < 1:
-                masks_loci.append(True)
-            else:
-                masks_loci.append(False)
+            if percent_match < 20:
+                masks_loci.add(i)
         masks_control[allele] = masks_loci
     return masks_control
