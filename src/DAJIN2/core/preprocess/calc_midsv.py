dfb68fa59382db04da033e05fba7d1c8b101749e
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-11-11 08:33:39 +0900

    update to add `QSCORE`

--- src/DAJIN2/core/preprocess/calc_midsv.py
+++ src/DAJIN2/core/preprocess/calc_midsv.py
@@ -3,11 +3,8 @@ from pathlib import Path
 import midsv
 
 
-def output_midsv(TEMPDIR, path_sam, DICT_ALLELE):
-    name_fasta = path_sam.stem.split("_")[-1]
-    if name_fasta not in set(DICT_ALLELE.keys()):
-        return
+def output_midsv(TEMPDIR, path_sam):
     sam = midsv.read_sam(path_sam)
-    midsv_jsonl = midsv.transform(sam, midsv=False, cssplit=True, qscore=False)
+    midsv_jsonl = midsv.transform(sam, midsv=False, cssplit=True, qscore=True)
     output_jsonl = Path(TEMPDIR, "midsv", f"{path_sam.stem}.jsonl")
     midsv.write_jsonl(midsv_jsonl, output_jsonl)
