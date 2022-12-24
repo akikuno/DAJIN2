e3f7b6448727432494e84aa05d6f004adbadf704
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-12-25 07:42:48 +0900

    rename test scripts

---
+++ tests/test_preprocess_correct_cssplits.py
@@ -0,0 +1,6 @@
+"""
+- repetitive dels
+- Shared mutation percentages in 5-mer
+    - sample:  [{"=A,=A,=A,=A,=A": [0.98,0.98,0.98,0.98,0.98,0.98]}, ]
+    - control: [{"=A,=A,=A,=A,=A": [0.98,0.98,0.98,0.98,0.98,0.98]}]
+"""
