996aa81730f244bf7d7a6e1c21bec987b59d5eee
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-12-26 14:42:27 +0900

    add test to `extract_indexes_with_both_ends_not_N`

--- misc/scripts/scratch_correct_cssplits.py
+++ misc/scripts/scratch_correct_cssplits.py
@@ -5,14 +5,43 @@ import re
 from itertools import chain
 from copy import deepcopy
 from collections import defaultdict
+
+from scipy import stats
 from scipy.spatial.distance import cosine
+
 from pathlib import Path
 
 import midsv
 
 """
 - 5-merでコントロールと比較して、コントロールにもあるプロファイルの場合はマッチで補正する
-    - 補正する前にすでにすべてがマッチなら即continue
     - 断片のリードで、端から続くNは無視
+    - 補正する前にすでにすべてがマッチなら即continue
 """
 
+
+def extract_indexes_with_both_ends_not_N(cssplits: str) -> tuple(int, int):
+    n_prefix = re.search(r"^(N,)+", cssplits)
+    left_idx = n_prefix.end() if n_prefix else 0
+    n_suffix = re.search(r"(,N)+$", cssplits)
+    right_idx = n_suffix.start() if n_suffix else len(cssplits)
+    # output index of splitted cssplits
+    left_idx = cssplits[:left_idx].count(",")
+    right_idx = cssplits.count(",") - cssplits[right_idx:].count(",")
+    return left_idx, right_idx
+
+
+# cssplits = "N,N,N,N,=A,=C,=G,=T"
+# cssplits = "N,N,=A,=C,=G,=T,N,N"
+# left_idx, right_idx = extract_indexes_with_both_ends_not_N(cssplits)
+# # cssplits.split(",")[left_idx]
+# # cssplits.split(",")[right_idx]
+
+# cssplits_sample = ["=A,=C,=G,=T", "*AT,-C,+A|G,=t"]
+
+# a = [0.01, 0.02, 0.03]
+# b = [0.97, 0.98, 0.99]
+# 1 - cosine(a, b)
+
+# _, pval = stats.ttest_ind(a, b, equal_var=False)
+# pval
--- tests/test_preprocess_correct_cssplits.py
+++ tests/test_preprocess_correct_cssplits.py
@@ -4,3 +4,33 @@
     - sample:  [{"=A,=A,=A,=A,=A": [0.98,0.98,0.98,0.98,0.98,0.98]}, ]
     - control: [{"=A,=A,=A,=A,=A": [0.98,0.98,0.98,0.98,0.98,0.98]}]
 """
+
+from misc.scripts import scratch_correct_cssplits
+
+
+def test_extract_indexes_with_both_ends_not_N___noN():
+    cssplits = "=A,=C,=G,=T"
+    test = scratch_correct_cssplits.extract_indexes_with_both_ends_not_N(cssplits)
+    answer = (0, 3)
+    assert test == answer
+
+
+def test_extract_indexes_with_both_ends_not_N___leftN():
+    cssplits = "N,N,N,N,=A,=C,=G,=T"
+    test = scratch_correct_cssplits.extract_indexes_with_both_ends_not_N(cssplits)
+    answer = (4, 7)
+    assert test == answer
+
+
+def test_extract_indexes_with_both_ends_not_N___rightN():
+    cssplits = "=A,=C,=G,=T,N,N,N,N"
+    test = scratch_correct_cssplits.extract_indexes_with_both_ends_not_N(cssplits)
+    answer = (0, 3)
+    assert test == answer
+
+
+def test_extract_indexes_with_both_ends_not_N___bothN():
+    cssplits = "N,N,=A,=C,=G,=T,=A,N,N"
+    test = scratch_correct_cssplits.extract_indexes_with_both_ends_not_N(cssplits)
+    answer = (2, 6)
+    assert test == answer
