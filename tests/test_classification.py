5eeece3c1186ffb943272d941b9cb5738d55b547
Akihiro Kuno <akikuno@users.noreply.github.com>
2023-01-12 10:49:53 +0900

    update tests of `calc_match`

--- tests/test_classification.py
+++ tests/test_classification.py
@@ -5,30 +5,30 @@ reload(classify)
 
 
 def test_calc_match_perfect():
-    test = classify.calc_match("=A,=C,=G,=T")
+    test = classify.calc_match("=A,=C,=G,=T", mutations={})
     answer = 1.0
     assert test == answer
 
 
 def test_calc_match_insertion():
-    test = classify.calc_match("=A,=C,+T|+T|=G,=T")
+    test = classify.calc_match("=A,=C,+T|+T|=G,=T", mutations={})
     answer = 0.5
     assert test == answer
 
 
 def test_calc_match_deletion():
-    test = classify.calc_match("=A,-C,-G,=T")
+    test = classify.calc_match("=A,-C,-G,=T", mutations={})
     answer = 0.5
     assert test == answer
 
 
 def test_calc_match_substitution():
-    test = classify.calc_match("=A,*CT,=G,=T")
+    test = classify.calc_match("=A,*CT,=G,=T", mutations={})
     answer = 0.75
     assert test == answer
 
 
 def test_calc_match_unknown():
-    test = classify.calc_match("=A,N,=G,=T")
+    test = classify.calc_match("=A,N,=G,=T", mutations={})
     answer = 0.75
     assert test == answer
