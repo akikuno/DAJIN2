d2b5fe7068d50dee62cd346f878ec62d820df882
Akihiro Kuno <akikuno@users.noreply.github.com>
2023-01-17 09:19:33 +0900

    wip

--- misc/scripts/2023-01-13-knocin-correct.py
+++ misc/scripts/2023-01-13-knocin-correct.py
@@ -18,6 +18,7 @@ from __future__ import annotations
     right-loxp: 2428
 結果
 """
+from difflib import get_close_matches
 import re
 from collections import Counter, defaultdict
 from copy import deepcopy
@@ -108,6 +109,10 @@ count_control
 
 # knockin_kmer = ["AGCTA", "TAACT"]
 
+###############################################################################
+# functions
+###############################################################################
+
 
 def extract_knockin_loci(TEMPDIR) -> defaultdict[set]:
     """
@@ -131,178 +136,214 @@ def extract_knockin_loci(TEMPDIR) -> defaultdict[set]:
     return knockin_alleles
 
 
-knockin_alleles = extract_knockin_loci(TEMPDIR)
-cssplits = cssplits_sample
-knockin_loci = knockin_alleles["flox"]
-sequence = FASTA_ALLELES["flox"]
-i = 1776
-
-
-def split_sequence_in_kmer(sequence: str):
+def split_sequence_in_5mer(sequence: str):
     sequence_kmer = dict()
     for i in range(len(sequence) - 5):
         sequence_kmer.update({sequence[i : i + 5]: i + 2})
     return sequence_kmer
 
 
-sequence_kmer = split_sequence_in_kmer(sequence)
+def get_5mer_of_knockin_loci(sequence: str, knockin_loci: list):
+    sequence_kmer = dict()
+    for i in knockin_loci:
+        sequence_kmer.update({sequence[i : i + 5]: i + 2})
+    return sequence_kmer
 
-import difflib
 
-similar_5mers = []
-for key in difflib.get_close_matches(sequence[i - 2 : i + 3], sequence_kmer.keys(), n=100, cutoff=0.0):
-    idx = sequence_kmer[key]
-    if set(range(idx - 2, idx + 3)) & knockin_loci:
-        continue
-    similar_5mers.append(idx)
-
-sequence[i - 2 : i + 3], i
-j = 1299
-sequence[j - 2 : j + 3]
-
-cssplits = cssplits_control
-transposed = [list(t) for t in zip(*cssplits)]
-count_control_5mer = []
-for i in similar_5mers:
-    cssplits_5mer = transposed[i - 2 : i + 3]
-    count = {"ins": [1] * 5, "del": [1] * 5, "sub": [1] * 5}
-    for j, cs in enumerate(cssplits_5mer):
-        counter = Counter(cs)
-        for key, cnt in counter.items():
-            if key.startswith("=") or key == "N" or re.search(r"a|c|g|t|n", key):
+def get_idx_of_similar_5mers(knockin_kmer: dict, sequence_kmer: dict) -> defaultdict(list):
+    idx_of_similar_5mers = defaultdict(list)
+    for kmer, locus in knockin_kmer.items():
+        idxes = []
+        for key in get_close_matches(kmer, sequence_kmer.keys(), n=100, cutoff=0.0):
+            idx = sequence_kmer[key]
+            if set(range(idx - 2, idx + 3)) & knockin_loci:
                 continue
-            if key.startswith("+"):
-                count["ins"][j] += cnt
-            elif key.startswith("-"):
-                count["del"][j] += cnt
-            elif key.startswith("*"):
-                count["sub"][j] += cnt
-    count_control_5mer.append(count)
+            idxes.append(idx)
+        idx_of_similar_5mers[locus] = idxes
+    return idx_of_similar_5mers
 
+
+def count_indel_5mer(cssplits_transposed, indexes) -> defaultdict(dict):
+    count_5mer = defaultdict(dict)
+    for i in indexes:
+        cssplits_5mer = cssplits_transposed[i - 2 : i + 3]
+        count = {"ins": [1] * 5, "del": [1] * 5, "sub": [1] * 5}
+        for j, cs in enumerate(cssplits_5mer):
+            counter = Counter(cs)
+            for key, cnt in counter.items():
+                if key.startswith("=") or key == "N" or re.search(r"a|c|g|t|n", key):
+                    continue
+                if key.startswith("+"):
+                    count["ins"][j] += cnt
+                elif key.startswith("-"):
+                    count["del"][j] += cnt
+                elif key.startswith("*"):
+                    count["sub"][j] += cnt
+        count_5mer[i] = count
+    return count_5mer
+
+
+def replace_errors_to_match(cssplits_sample, sequence_errors, sequence):
+    cssplits_replaced = []
+    for cssplits in cssplits_sample:
+        cssplits_copy = deepcopy(cssplits)
+        for idx, error in sequence_errors.items():
+            cssplits_5mer = cssplits_copy[idx - 2 : idx + 3]
+            for j, mer in enumerate(cssplits_5mer):
+                match_seq = "=" + sequence[idx - 2 + j]
+                if "ins" in error and mer.startswith("+"):
+                    cssplits_5mer[j] = match_seq
+                if "del" in error and mer.startswith("-"):
+                    cssplits_5mer[j] = match_seq
+                if "del" in error and mer.startswith("*"):
+                    cssplits_5mer[j] = match_seq
+            cssplits_copy[idx - 2 : idx + 3] = cssplits_5mer
+        cssplits_replaced.append(cssplits_copy)
+    return cssplits_replaced
+
+
+##########################################################
+knockin_alleles = extract_knockin_loci(TEMPDIR)
+knockin_loci = knockin_alleles["flox"]
+sequence = FASTA_ALLELES["flox"]
 i = 1776
-cssplits = cssplits_sample
-transposed = [list(t) for t in zip(*cssplits)]
-count_indels_5mer = []
-count = {"ins": [1] * 5, "del": [1] * 5, "sub": [1] * 5}
-cssplits_5mer = transposed[i - 2 : i + 3]
-for j, cs in enumerate(cssplits_5mer):
-    counter = Counter(cs)
-    for key, cnt in counter.items():
-        if key.startswith("=") or key == "N" or re.search(r"a|c|g|t|n", key):
-            continue
-        if key.startswith("+"):
-            count["ins"][j] += cnt
-        elif key.startswith("-"):
-            count["del"][j] += cnt
-        elif key.startswith("*"):
-            count["sub"][j] += cnt
 
-count_sample_5mer_knockin = deepcopy(count)
+"""
+コントロールとの類似配列において、エラーの相同性が高いものをシークエンスエラーとする
+"""
 
-count_sample_5mer_knockin
-count_control_5mer[0]
+
+knockin_kmer = get_5mer_of_knockin_loci(sequence, knockin_loci)
+sequence_kmer = split_sequence_in_5mer(sequence)
+
+idx_of_similar_5mers = get_idx_of_similar_5mers(knockin_kmer, sequence_kmer)
+
+# i = 1776
+# sequence[i - 2 : i + 3], i
+# j = 1299
+# sequence[j - 2 : j + 3]
+# idx_of_similar_5mers[i]
+
+
+count_5mer_similar_sequences = defaultdict(dict)
+cssplits_transposed = [list(t) for t in zip(*cssplits_control)]
+for i, indexes in idx_of_similar_5mers.items():
+    count_5mer_similar_sequences[i] = count_indel_5mer(cssplits_transposed, indexes)
+
+"""
+Knock-in配列のエラープロファイル
+"""
+# i = 1776
+transposed_cssplits = [list(t) for t in zip(*cssplits_sample)]
+count_5mer_knockin = count_indel_5mer(transposed_cssplits, knockin_loci)
+
+
 coverage_sample = len(midsv_sample)
 coverage_control = len(midsv_control)
 
-results = []
-for idx, count_control in enumerate(count_control_5mer):
-    # for op in ["ins", "del", "sub"]:
-    op = "sub"
-    cont = [c / coverage_control for c in count_control[op]]
-    samp = [c / coverage_sample for c in count_sample_5mer_knockin[op]]
-    distance = 1 - cosine(samp, cont)
-    _, pvalue = stats.ttest_ind(samp, cont, equal_var=False)
-    results.append((idx, distance, pvalue))
-    # if distance > 0.7 and pvalue > 0.01:
-    #     results.append((idx, distance, pvalue))
+sequence_errors = defaultdict(set)
 
-count_control = count_control_5mer[73]
-count_control
-sorted(results, key=lambda x: -x[1])[:10]
-sorted(results, key=lambda x: -x[2])[:10]
-
-"""
-Knockin配列内において、エラーの相同性が高いものはシークエンスエラーとする
-"""
-
-knockin_loci
-
-cssplits = cssplits_sample
-transposed = [list(t) for t in zip(*cssplits)]
-count_knockin_5mer = defaultdict(dict)
-for i in knockin_loci:
-    cssplits_5mer = transposed[i - 2 : i + 3]
-    count = {"ins": [1] * 5, "del": [1] * 5, "sub": [1] * 5}
-    for j, cs in enumerate(cssplits_5mer):
-        counter = Counter(cs)
-        for key, cnt in counter.items():
-            if key.startswith("=") or key == "N" or re.search(r"a|c|g|t|n", key):
-                continue
-            if key.startswith("+"):
-                count["ins"][j] += cnt
-            elif key.startswith("-"):
-                count["del"][j] += cnt
-            elif key.startswith("*"):
-                count["sub"][j] += cnt
-    count_knockin_5mer[i] = count
-
-i = 2444
-results = []
-for i in knockin_loci:
-    for j in knockin_loci:
-        if j in range(i - 4, i + 5):
-            continue
-        x = count_knockin_5mer[i]
-        y = count_knockin_5mer[j]
-        op = "del"
-        x = [c / coverage_sample for c in x[op]]
-        y = [c / coverage_sample for c in y[op]]
-        distance = 1 - cosine(x, y)
-        _, pvalue = stats.ttest_ind(x, y, equal_var=False)
-        # results.append((j, distance, pvalue))
-        if distance > 0.7 and pvalue > 0.01:
-            results.append((j, distance, pvalue))
-
-sorted(results, key=lambda x: -x[1])[:10]
-sorted(results, key=lambda x: -x[2])[:10]
-results
-sequence[i - 2 : i + 3]
-sequence[j - 2 : j + 3]
-sequence[i - 2 : j + 3]
-count_knockin_5mer[2492]
-
-
-def extract_sequence_errors(count_5mer_sample, count_5mer_control, coverage_sample, coverage_control):
-    sequence_errors = [set() for _ in range(len(count_5mer_sample))]
-    for i in range(len(sequence_errors)):
-        for ids in ["ins", "del", "sub"]:
-            samp = [c / coverage_sample for c in count_5mer_sample[i][ids]]
-            cont = [c / coverage_control for c in count_5mer_control[i][ids]]
+for i, count_knockin in count_5mer_knockin.items():
+    samp_mutation = defaultdict(list)
+    for mutation in ["ins", "del", "sub"]:
+        samp_mutation[mutation] = [c / coverage_sample for c in count_knockin[mutation]]
+    count_control = count_5mer_similar_sequences[i]
+    for j, count in count_control.items():
+        for mutation in ["ins", "del", "sub"]:
+            samp = samp_mutation[mutation]
+            cont = [c / coverage_control for c in count[mutation]]
             distance = 1 - cosine(samp, cont)
             _, pvalue = stats.ttest_ind(samp, cont, equal_var=False)
-            if distance > 0.95 and pvalue > 0.05:
-                sequence_errors[i].add(ids)
-    return sequence_errors
+            if distance > 0.7 and pvalue > 0.01:
+                sequence_errors[i].add(mutation)
 
 
-# def count_indels_kmer_around_knockin(cssplits: list[list[str]], knockin_loci: list) -> list[dict]:
-#     transposed = [list(t) for t in zip(*cssplits)]
-#     count_indels_kmer = []
-#     for i in knockin_loci:
-#         count = {"ins": [1] * 5, "del": [1] * 5, "sub": [1] * 5}
-#         if (i - 2) < 0 or (i + 3) > len(transposed):
+###############################################################
+# Knock-in配列中のシークエンスエラーを補正する
+###############################################################
+
+cssplits_replaced = replace_errors_to_atmark(cssplits_sample, sequence_errors, sequence)
+
+
+# 確認
+
+idx = 2444  # -A
+idx = 1776  # *GA
+# left_start = 1733
+count_sample = defaultdict(int)
+for cs in midsv_sample:
+    x = cs["CSSPLIT"].split(",")[idx]
+    count_sample[x] += 1
+
+count_control = defaultdict(int)
+for cs in midsv_control:
+    x = cs["CSSPLIT"].split(",")[idx]
+    count_control[x] += 1
+
+
+count_replaced = defaultdict(int)
+for cs in cssplits_replaced:
+    x = cs[idx]
+    count_replaced[x] += 1
+
+
+count_sample
+count_control
+count_replaced
+
+
+# i = 1776
+# count_knockin = count_5mer_knockin[i]
+# sequence_errors[i]
+# count_control = count_control_5mer[73]
+# count_control
+# sorted(results, key=lambda x: -x[1])[:10]
+# sorted(results, key=lambda x: -x[2])[:10]
+
+###########################################################
+# Knockin配列内において、エラーの相同性が高いものをシークエンスエラーとする
+###########################################################
+
+# cssplits = cssplits_sample
+# transposed = [list(t) for t in zip(*cssplits)]
+# count_knockin_5mer = defaultdict(dict)
+# for i in knockin_loci:
+#     cssplits_5mer = transposed[i - 2 : i + 3]
+#     count = {"ins": [1] * 5, "del": [1] * 5, "sub": [1] * 5}
+#     for j, cs in enumerate(cssplits_5mer):
+#         counter = Counter(cs)
+#         for key, cnt in counter.items():
+#             if key.startswith("=") or key == "N" or re.search(r"a|c|g|t|n", key):
+#                 continue
+#             if key.startswith("+"):
+#                 count["ins"][j] += cnt
+#             elif key.startswith("-"):
+#                 count["del"][j] += cnt
+#             elif key.startswith("*"):
+#                 count["sub"][j] += cnt
+#     count_knockin_5mer[i] = count
+
+# i = 2444
+# results = []
+# for i in knockin_loci:
+#     for j in knockin_loci:
+#         if j in range(i - 4, i + 5):
 #             continue
-#         cssplits_kmer = transposed[i - 2 : i + 3]
-#         for j, cs in enumerate(cssplits_kmer):
-#             counter = Counter(cs)
-#             for key, cnt in counter.items():
-#                 if key.startswith("=") or key == "N" or re.search(r"a|c|g|t|n", key):
-#                     continue
-#                 if key.startswith("+"):
-#                     count["ins"][j] += cnt
-#                 elif key.startswith("-"):
-#                     count["del"][j] += cnt
-#                 elif key.startswith("*"):
-#                     count["sub"][j] += cnt
-#         count_indels_kmer.append(count)
-#     return count_indels_kmer
+#         x = count_5mer_knockin[i]
+#         y = count_5mer_knockin[j]
+#         op = "sub"
+#         x = [c / coverage_sample for c in x[op]]
+#         y = [c / coverage_sample for c in y[op]]
+#         distance = 1 - cosine(x, y)
+#         _, pvalue = stats.ttest_ind(x, y, equal_var=False)
+#         # results.append((j, distance, pvalue))
+#         if distance > 0.7 and pvalue > 0.01:
+#             results.append((j, distance, pvalue))
+
+# sorted(results, key=lambda x: -x[1])[:10]
+# sorted(results, key=lambda x: -x[2])[:10]
+# results
+# sequence[i - 2 : i + 3]
+# sequence[j - 2 : j + 3]
+# sequence[i - 2 : j + 3]
+# count_knockin_5mer[2492]
