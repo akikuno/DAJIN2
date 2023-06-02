0fe3e1437a9f2c205d80d1959a5a3ecb2b39bd12
akikuno <akikuno@users.noreply.github.com>
2023-06-03 08:13:34 +0900

    sort x labels

--- src/DAJIN2/postprocess/report.py
+++ src/DAJIN2/postprocess/report.py
@@ -8,10 +8,10 @@ import pandas as pd
 import plotly.express as px
 
 
-def all_info(batch_directory: Path) -> pd.DataFrame:
+def extract_all_info(batch_directory: Path) -> pd.DataFrame:
     df_results = pd.DataFrame()
     colum = ["SAMPLE", "QNAME", "NAME", "READNUM", "PERCENT", "LABEL", "ALLELE"]
-    for path_result in batch_directory.iterdir():
+    for path_result in sorted(batch_directory.iterdir()):
         sample_name = path_result.stem
         result_jsonl = midsv.read_jsonl(path_result)
         df_clust_sample = pd.DataFrame(result_jsonl)
@@ -21,7 +21,7 @@ def all_info(batch_directory: Path) -> pd.DataFrame:
     return df_results
 
 
-def summary_info(df_all: pd.DataFrame) -> pd.DataFrame:
+def summarize_info(df_all: pd.DataFrame) -> pd.DataFrame:
     df_summary = df_all.drop(columns=["QNAME", "LABEL", "ALLELE"]).drop_duplicates()
     return df_summary
 
@@ -41,6 +41,7 @@ def output_plot(df_summary: pd.DataFrame, report_directory: Path):
         labels={"SAMPLE": "Samples", "PERCENT": "% of reads", "ALLELETYPE": "Alelle type"},
     )
     fig.update_traces(textposition="inside", cliponaxis=False)
+    fig.update_xaxes(categoryorder="category ascending")
     output_filename = str(Path(report_directory, "read_plot"))
     fig.write_html(f"{output_filename}.html")
     # if kaleido is installed, output a pdf
@@ -53,10 +54,10 @@ def output_plot(df_summary: pd.DataFrame, report_directory: Path):
 def report(name: str):
     report_directory = Path("DAJINResults", name)
     report_directory.mkdir(exist_ok=True, parents=True)
-    for dir in Path("DAJINResults", ".tempdir", name, "report").iterdir():
-        shutil.copytree(dir, Path(report_directory, dir.stem), dirs_exist_ok=True)
-    df_all = all_info(Path("DAJINResults", ".tempdir", name, "result"))
+    for directory in Path("DAJINResults", ".tempdir", name, "report").iterdir():
+        shutil.copytree(directory, Path(report_directory, directory.stem), dirs_exist_ok=True)
+    df_all = extract_all_info(Path("DAJINResults", ".tempdir", name, "result"))
     df_all.to_csv(Path(report_directory, "read_all.csv"), index=False)
-    df_summary = summary_info(df_all)
+    df_summary = summarize_info(df_all)
     df_summary.to_csv(Path(report_directory, "read_summary.csv"), index=False)
     output_plot(df_summary, report_directory)
