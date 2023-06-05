3e095996ad0a9e49c6231e1590f0d4b2f8b001ac
akikuno <akikuno@users.noreply.github.com>
2023-06-05 14:25:53 +0900

    remove `misc`

--- misc/experiments/done/debug-stx2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cat examples/del-stx2/design_stx2.fa | grep "control" -A1 >tmpstx.fa
-zcat examples/del-stx2/barcode25.fq.gz | grep 3f7aff2243b8 -A3 >tmp3f7aff2243b8.fq
-minimap2 -ax map-ont tmpstx.fa tmp3f7aff2243b8.fq --cs=long >tmp3f7aff2243b8.sam
-
-cat DAJINResults/.tempdir/stx2-deletion/sam/barcode25_control.sam | grep 3f7aff2243b8 >tmp3f7aff2243b8_DAJIN2.sam
