88280db5d5a768f23d4af396bd35cd5cb2e7f9ca
akikuno <akikuno@users.noreply.github.com>
2023-06-05 14:33:28 +0900

    bump `0.2.0-alpha`

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt") as requirements_file:
 
 setuptools.setup(
     name="DAJIN2",
-    version="0.1.33",
+    version="0.2.0",
     author="Akihiro Kuno",
     author_email="akuno@md.tsukuba.ac.jp",
     description="One-step genotyping tools for Nanopore amplicon sequencing",
