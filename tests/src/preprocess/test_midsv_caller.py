82067189d7e34aecefe88d90303b79f831380790
akikuno <akikuno@users.noreply.github.com>
2023-08-16 13:07:10 +0900

    refactoring and adding tests to `midsv_caller.py`

--- tests/src/preprocess/test_midsv_caller.py
+++ tests/src/preprocess/test_midsv_caller.py
@@ -159,6 +159,24 @@ def test_replace_n_to_d():
     assert result == [{"CSSPLIT": "A,B,C,D,E"}]
 
 
+def test_replace_n_to_d_multiple_samples():
+    midsv_sample = [{"CSSPLIT": "N,N,N,=A,N,=C,N,N"}, {"CSSPLIT": "N,N,=A,N,N,=C,=C,N"}]
+    sequence = "GCAACCCC"
+    test = replace_n_to_d(midsv_sample, sequence)
+    test = list(test)
+    answer = [{"CSSPLIT": "N,N,N,=A,-C,=C,N,N"}, {"CSSPLIT": "N,N,=A,-A,-C,=C,=C,N"}]
+    assert test == answer
+
+
+def test_replace_n_to_d_large_n():
+    midsv_sample = [{"CSSPLIT": "N,N,N,N,N,N,=C,N,=A"}]
+    sequence = "GCAACCCCA"
+    test = replace_n_to_d(midsv_sample, sequence)
+    test = list(test)
+    answer = [{"CSSPLIT": "N,N,N,N,N,N,=C,-C,=A"}]
+    assert test == answer
+
+
 ###########################################################
 # convert_flag_to_strand
 ###########################################################
