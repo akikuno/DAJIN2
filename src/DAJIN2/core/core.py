cb450a1ff7e1fd1816c7d403ee17a7456ae758da
akikuno <akikuno@users.noreply.github.com>
2023-08-18 12:57:51 +0900

    update memory limits

--- src/DAJIN2/core/core.py
+++ src/DAJIN2/core/core.py
@@ -21,8 +21,8 @@ from DAJIN2.utils.config import TEMP_ROOT_DIR
 # available_memory = psutil.virtual_memory().available
 available_memory = os.sysconf("SC_PAGE_SIZE") * os.sysconf("SC_PHYS_PAGES")
 _, hard_limit = resource.getrlimit(resource.RLIMIT_DATA)
-if hard_limit == resource.RLIM_INFINITY:
-    hard_limit = available_memory
+# if hard_limit == resource.RLIM_INFINITY:
+#     hard_limit = available_memory
 # Ensure that the new limit does not exceed the hard limit
 limit = min(int(available_memory * 0.9), hard_limit)
 resource.setrlimit(resource.RLIMIT_DATA, (limit, limit))
