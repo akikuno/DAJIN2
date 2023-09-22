bfa190628454985c2000f9b0e46bd4b5fa1e5fa4
akikuno <akikuno@users.noreply.github.com>
2023-09-23 08:56:56 +0900

    update error message: made it more explicit, indicating which errors occurred in which samples.

--- src/DAJIN2/utils/multiprocess.py
+++ src/DAJIN2/utils/multiprocess.py
@@ -1,15 +1,19 @@
 from __future__ import annotations
 
+import io
 import sys
-import traceback
 import logging
+import traceback
 
-from typing import Generator
 from itertools import islice
 from multiprocessing import Process, Queue
 
+from typing import Generator
+from collections.abc import Callable
 
-def generate_chunks(iterable, chunk_size: int) -> Generator(tuple):
+
+def generate_chunks(iterable, chunk_size: int = 1) -> Generator[tuple]:
+    """Yield successive n-sized chunks from iterable."""
     iterator = iter(iterable)
     while True:
         chunk = tuple(islice(iterator, chunk_size))
@@ -18,27 +22,45 @@ def generate_chunks(iterable, chunk_size: int) -> Generator(tuple):
         yield chunk
 
 
-def run(function, arguments: list[dict], num_workers: int = 1) -> None:
+def target(function: Callable, arg: dict, queue: Queue[str]) -> None:
+    """Run a function with a single argument, and handle any exceptions."""
+    try:
+        # Redirect stderr to suppress traceback printing
+        original_stderr = sys.stderr
+        sys.stderr = io.StringIO()
+        function(arg)
+    except Exception:
+        # Reset stderr back to original
+        sys.stderr = original_stderr
+        queue.put(f"An unexpected error occurred at {arg['sample']}\n{traceback.format_exc()}")
+        raise  # Re-raise the exception to be caught in the parent process
+    finally:
+        # Ensure stderr is reset back to original even if no error occurs
+        sys.stderr = original_stderr
+
+
+def run(function: Callable, arguments: list[dict], num_workers: int = 1) -> None:
+    """Run a function in parallel over a list of arguments."""
     logger = logging.getLogger(__name__)
 
-    q = Queue()
-
-    def target(arg):
-        try:
-            function(arg)
-        except Exception:
-            q.put(traceback.format_exc())
-            sys.exit(1)
+    queue = Queue()
 
     arguments_chunked = generate_chunks(arguments, num_workers)
     for args in arguments_chunked:
-        processes = [Process(target=target, args=(arg,)) for arg in args]
+        processes = [
+            Process(
+                target=target,
+                args=(function, arg, queue),
+                name=f"An unexpected error occurred at {arg['sample']}",
+            )
+            for arg in args
+        ]
 
-        for p in processes:
-            p.start()
+        for process in processes:
+            process.start()
 
-        for p in processes:
-            p.join()
-            if p.exitcode == 1:
-                logger.error(f"An unexpected error occurred: {q.get()}")
-                sys.exit(1)
+        for process in processes:
+            process.join()
+            if process.exitcode == 1:
+                logger.error(queue.get())
+                break
