1b9cfdf5da7351b58f16aee817e31acb4b85e170
Akihiro Kuno <akikuno@github.com>
2023-11-12 14:13:18 +0900

    rename `label_handler` to `label_updator`

--- tests/src/clustering/test_label_updator.py
+++ tests/src/clustering/test_label_updator.py
@@ -1,15 +1,15 @@
-from src.DAJIN2.core.clustering import label_handler
+from src.DAJIN2.core.clustering import label_updator
 
 
 def test_relabel_with_consective_order():
     labels = [1, 4, 4, 4, 2, 2, 2]
-    test = label_handler.relabel_with_consective_order(labels)
+    test = label_updator.relabel_with_consective_order(labels)
     answer = [1, 2, 2, 2, 3, 3, 3]
     assert test == answer
 
 
 def test_relabel_with_consective_order_start_1():
     labels = [1, 4, 4, 4, 2, 2, 2]
-    test = label_handler.relabel_with_consective_order(labels, start=1)
+    test = label_updator.relabel_with_consective_order(labels, start=1)
     answer = [2, 3, 3, 3, 4, 4, 4]
     assert test == answer
