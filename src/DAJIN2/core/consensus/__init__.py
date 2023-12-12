b5bdd53190fb769f74749f7a9ea19f65ea58e23a
akikuno <akikuno@users.noreply.github.com>
2023-12-12 14:26:30 +0900

    remove homopolymer_masker

--- src/DAJIN2/core/consensus/__init__.py
+++ src/DAJIN2/core/consensus/__init__.py
@@ -3,4 +3,3 @@ from DAJIN2.core.consensus.name_handler import call_allele_name
 from DAJIN2.core.consensus.name_handler import update_key_by_allele_name
 from DAJIN2.core.consensus.name_handler import add_key_by_allele_name
 from DAJIN2.core.consensus.clust_formatter import subset_clust, cache_mutation_loci
-from DAJIN2.core.consensus.homopolymer_masker import extract_index_of_homopolymer_errors
