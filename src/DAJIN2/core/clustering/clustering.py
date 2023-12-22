d85c22c4a5e6267b9ef01b5849e9ec5ade06b199
akikuno <akikuno@users.noreply.github.com>
2023-12-22 09:01:46 +0900

    changed clustering algorithm from KMeans to MiniBatchKMeans to handle larger dataset

--- src/DAJIN2/core/clustering/clustering.py
+++ src/DAJIN2/core/clustering/clustering.py
@@ -6,7 +6,7 @@ from collections import Counter
 
 import numpy as np
 from sklearn import metrics
-from sklearn.cluster import KMeans
+from sklearn.cluster import MiniBatchKMeans
 from scipy.sparse import csr_matrix, spmatrix
 
 from DAJIN2.utils import io, config
@@ -26,7 +26,7 @@ def optimize_labels(X: spmatrix, coverage_sample, coverage_control) -> list[int]
     labels_previous = list(range(coverage_sample))
     for i in range(1, coverage_sample):
         np.random.seed(seed=1)
-        labels_all = KMeans(n_clusters=i, random_state=1, n_init="auto").fit_predict(X).tolist()
+        labels_all = MiniBatchKMeans(n_clusters=i, random_state=1, n_init="auto").fit_predict(X).tolist()
         labels_sample = labels_all[:coverage_sample]
         labels_control = labels_all[coverage_sample:]
         labels_current = merge_labels(labels_control, labels_sample)
@@ -53,7 +53,7 @@ def return_labels(path_score_sample: Path, path_score_control: Path, path_sample
     score_control = list(io.read_jsonl(path_score_control))
     X_control = csr_matrix(score_control)
     # subset to 1000 reads of controls in the most common cluster to remove outliers and reduce computation time
-    labels_control = KMeans(n_clusters=2, random_state=1, n_init="auto").fit_predict(X_control)
+    labels_control = MiniBatchKMeans(n_clusters=2, random_state=1, n_init="auto").fit_predict(X_control)
     label_most_common = get_label_most_common(labels_control)
     scores_control_subset = subset_scores(labels_control, io.read_jsonl(path_score_control), label_most_common, 1000)
     scores_sample = list(io.read_jsonl(path_score_sample))
