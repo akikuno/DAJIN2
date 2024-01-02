217355d46bf69d5a7e03f775077d8124faea1982
akikuno <akikuno@users.noreply.github.com>
2024-01-02 17:40:30 +0900

    remove underscore of the functions

--- src/DAJIN2/core/preprocess/genome_fetcher.py
+++ src/DAJIN2/core/preprocess/genome_fetcher.py
@@ -3,12 +3,12 @@ from __future__ import annotations
 from urllib.request import urlopen
 
 
-def _fetch_seq_coordinates(genome: str, blat_url: str, seq: str) -> dict:
+def fetch_seq_coordinates(genome: str, blat_url: str, seq: str) -> dict:
     url = f"{blat_url}?db={genome}&type=BLAT&userSeq={seq}"
     response = urlopen(url).read().decode("utf8").split("\n")
     matches = [x for x in response if "100.0%" in x]
     if not matches:
-        raise ValueError(f"{seq} is not found in {genome}")
+        raise ValueError(f"{seq[:10]}... is not found in {genome}")
     chrom, strand, start, end, _ = matches[0].split()[-5:]
     return {"chrom": chrom, "strand": strand, "start": int(start), "end": int(end)}
 
@@ -18,8 +18,8 @@ def fetch_coordinates(genome_coordinates: dict, genome_urls: dict, seq: str) -> 
     blat_url = genome_urls["blat"]
 
     seq_start, seq_end = seq[:1000], seq[-1000:]
-    coordinate_start = _fetch_seq_coordinates(genome, blat_url, seq_start)
-    coordinate_end = _fetch_seq_coordinates(genome, blat_url, seq_end)
+    coordinate_start = fetch_seq_coordinates(genome, blat_url, seq_start)
+    coordinate_end = fetch_seq_coordinates(genome, blat_url, seq_end)
 
     chromosome, strand = coordinate_start["chrom"], coordinate_start["strand"]
     if strand == "+":
