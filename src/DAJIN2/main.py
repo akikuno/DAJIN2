fa18289cd6f0a7fa54cb265225ff9d926ecb0ba7
akikuno <akikuno@users.noreply.github.com>
2024-02-05 17:29:51 +0900

    Bump v0.4.1

--- src/DAJIN2/main.py
+++ src/DAJIN2/main.py
@@ -20,7 +20,7 @@ from DAJIN2.core import core
 from DAJIN2.utils import io, config, report_generator, input_validator, multiprocess
 
 
-DAJIN_VERSION = "0.4.0"
+DAJIN_VERSION = "0.4.1"
 
 
 def generate_report(name: str) -> None:
