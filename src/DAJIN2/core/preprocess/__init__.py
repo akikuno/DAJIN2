9d558304609935f9d3320cc1f6d7b3a46168d9e2
akikuno <akikuno@users.noreply.github.com>
2024-03-22 14:46:19 +0900

    Refactor `preprocess.__init__.py`

--- src/DAJIN2/core/preprocess/__init__.py
+++ src/DAJIN2/core/preprocess/__init__.py
@@ -1,11 +1,8 @@
-from DAJIN2.core.preprocess import (
-    fastx_parser,
-    genome_fetcher,
-    cache_checker,
-    directories,
-)
-
+from DAJIN2.core.preprocess.cache_checker import exists_cached_hash, exists_cached_genome
+from DAJIN2.core.preprocess.genome_fetcher import fetch_coordinates, fetch_chromosome_size
 from DAJIN2.core.preprocess.mapping import generate_sam
+from DAJIN2.core.preprocess.directory_manager import create_temporal_directories, create_report_directories
+from DAJIN2.core.preprocess.input_formatter import format_inputs
 from DAJIN2.core.preprocess.midsv_caller import generate_midsv
 from DAJIN2.core.preprocess.knockin_handler import extract_knockin_loci
 from DAJIN2.core.preprocess.mutation_extractor import cache_mutation_loci
