3eef49a622b52bc5a79c4b260fecb8eb9d43fb05
akikuno <akikuno@users.noreply.github.com>
2024-04-24 15:14:26 +0900

    Bump v0.4.6

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt") as requirements_file:
 
 setuptools.setup(
     name="DAJIN2",
-    version="0.4.5",
+    version="0.4.6",
     author="Akihiro Kuno",
     author_email="akuno@md.tsukuba.ac.jp",
     description="One-step genotyping tools for targeted long-read sequencing",
