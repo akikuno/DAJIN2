28a5eaa4557e3beb76854f8e7c3d9c9efc52c1ee
akikuno <akikuno@users.noreply.github.com>
2024-05-13 14:36:10 +0900

    Refactoring typehints

--- src/DAJIN2/core/preprocess/homopolymer_handler.py
+++ src/DAJIN2/core/preprocess/homopolymer_handler.py
@@ -33,7 +33,7 @@ def extract_errors(
     indels_normalized_sample: dict[str, np.array],
     indels_normalized_control: dict[str, np.array],
     candidate_loci: dict[set],
-) -> dict[str, set(int)]:
+) -> dict[str, set[int]]:
     errors_in_homopolymer = dict()
     for mut in ["+", "-", "*"]:
         repeat_regions = get_repeat_regions(sequence, candidate_loci[mut])
