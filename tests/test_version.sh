a0b39c929ce0b1b49d8a3724cbb77d6d788c0baa
akikuno <akikuno@users.noreply.github.com>
2024-05-22 09:44:20 +0900

    Add pip installation

--- tests/test_version.sh
+++ tests/test_version.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+pip install -qU pip . || exit 1
+
 PIP_VERSION=$(echo $(DAJIN2 --version) | awk '{print $NF}')
-UTIL_VERSION=$(grep "DAJIN_VERSION" DAJIN2/src/DAJIN2/utils/config.py | awk '{print $NF}' | tr -d '"')
+UTIL_VERSION=$(grep "DAJIN_VERSION" src/DAJIN2/utils/config.py | awk '{print $NF}' | tr -d '"')
+
+if [ -z "$PIP_VERSION" ]; then
+    echo "DAJIN2 is not installed"
+    exit 1
+fi
+
+if [ -z "$UTIL_VERSION" ]; then
+    echo "src/DAJIN2/utils/config.py is not found"
+    exit 1
+fi
 
 if [ "$PIP_VERSION" != "$UTIL_VERSION" ]; then
     echo "Version mismatch: $PIP_VERSION != $UTIL_VERSION"
