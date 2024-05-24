e671b5c84b4cf522faf51823e36fe075b049efcf
akikuno <akikuno@users.noreply.github.com>
2024-05-24 10:49:11 +0900

    Configure `clustering.extract_labels` so that alleles with a low number of reads (0.05% or fewer or 5 reads or fewer) are not clustered.

--- src/DAJIN2/core/clustering/clustering.py
+++ src/DAJIN2/core/clustering/clustering.py
@@ -27,11 +27,10 @@ def count_number_of_clusters(labels_control: list[int], coverage_control: int) -
     return sum(1 for control_reads in Counter(labels_control).values() if control_reads / coverage_control > 0.01)
 
 
-def optimize_labels(X: spmatrix, coverage_sample: int, coverage_control: int) -> list[int]:
-    labels_previous = list(range(coverage_sample))
-    min_cluster_size = int(coverage_sample * 0.005)  # 0.5% of the reads
+def optimize_labels(X: spmatrix, coverage_sample: int, coverage_control: int, min_cluster_size: int) -> list[int]:
+    labels_previous = [1 for _ in range(coverage_sample)]
 
-    for i in range(1, coverage_sample):
+    for i in range(2, coverage_sample):
         np.random.seed(seed=1)
         labels_all = BisectingKMeans(n_clusters=i, random_state=1).fit_predict(X).tolist()
 
@@ -63,21 +62,31 @@ def get_label_most_common(labels: list[int]) -> int:
 
 
 def return_labels(
-    path_score_sample: Path, path_score_control: Path, path_sample: Path, strand_bias_in_control: bool
+    path_score_sample: Path,
+    path_score_control: Path,
+    path_sample: Path,
+    strand_bias_in_control: bool,
+    min_cluster_size: int,
 ) -> list[int]:
     np.random.seed(seed=1)
+
     score_control = list(io.read_jsonl(path_score_control))
     X_control = csr_matrix(score_control)
+
     """Subset to 1000 reads of controls in the most common cluster to remove outliers and reduce computation time"""
     labels_control = BisectingKMeans(n_clusters=2, random_state=1).fit_predict(X_control)
     label_most_common = get_label_most_common(labels_control)
     scores_control_subset = subset_scores(labels_control, io.read_jsonl(path_score_control), label_most_common, 1000)
+
     scores_sample = list(io.read_jsonl(path_score_sample))
     X = csr_matrix(list(chain(scores_sample, scores_control_subset)))
+
     coverage_sample = io.count_newlines(path_score_sample)
     coverage_control = len(scores_control_subset)
-    labels = optimize_labels(X, coverage_sample, coverage_control)
+    labels = optimize_labels(X, coverage_sample, coverage_control, min_cluster_size)
+
     """Re-allocate clusters with strand bias to clusters without strand bias"""
     if strand_bias_in_control is False:
         labels = remove_biased_clusters(path_sample, path_score_sample, labels)
+
     return labels
--- src/DAJIN2/core/clustering/label_extractor.py
+++ src/DAJIN2/core/clustering/label_extractor.py
@@ -17,6 +17,7 @@ def extract_labels(classif_sample, TEMPDIR, SAMPLE_NAME, CONTROL_NAME) -> list[d
     max_label = 1
     strand_bias = is_strand_bias(Path(TEMPDIR, CONTROL_NAME, "midsv", "control.json"))
     classif_sample.sort(key=lambda x: x["ALLELE"])
+    min_samples = int(len(classif_sample) * 0.5 // 100)  # 0.5% of the samples
     for allele, group in groupby(classif_sample, key=lambda x: x["ALLELE"]):
         """Cache data to temporary files"""
         if Path(TEMPDIR, CONTROL_NAME, "midsv", f"{allele}.json").exists():
@@ -37,9 +38,9 @@ def extract_labels(classif_sample, TEMPDIR, SAMPLE_NAME, CONTROL_NAME) -> list[d
         """Skip clustering when the number of reads is too small or there is no mutation."""
         read_numbers = io.count_newlines(path_sample)
         is_no_mutation = all(m == set() for m in mutation_loci)
-        if read_numbers <= 5 or is_no_mutation:
-            max_label += 1
+        if read_numbers < max(5, min_samples) or is_no_mutation:
             labels_result.extend([max_label] * read_numbers)
+            max_label += 1
             continue
 
         """Calculate scores to temporary files."""
@@ -54,7 +55,7 @@ def extract_labels(classif_sample, TEMPDIR, SAMPLE_NAME, CONTROL_NAME) -> list[d
         io.write_jsonl(data=scores_control, file_path=path_score_control)
 
         """Extract labels."""
-        labels = return_labels(path_score_sample, path_score_control, path_sample, strand_bias)
+        labels = return_labels(path_score_sample, path_score_control, path_sample, strand_bias, min_samples)
         labels_result += relabel_with_consective_order(labels, start=max_label)
 
         """Remove temporary files."""
