b2c5116777aa207c1c8ae304ea6ad569d402a9cc
akikuno <akikuno@users.noreply.github.com>
2024-06-06 16:40:27 +0900

    Bump v0.5.1

--- src/DAJIN2/utils/config.py
+++ src/DAJIN2/utils/config.py
@@ -9,7 +9,7 @@ from pathlib import Path
 
 from sklearn.exceptions import ConvergenceWarning
 
-DAJIN_VERSION = "0.5.0"
+DAJIN_VERSION = "0.5.1"
 DAJIN_RESULTS_DIR = Path("DAJIN_Results")
 TEMP_ROOT_DIR = Path(DAJIN_RESULTS_DIR, ".tempdir")
 
