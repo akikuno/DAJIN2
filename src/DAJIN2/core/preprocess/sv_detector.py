ddb881ea24a12da71b815375869f482e9800668b
akikuno <akikuno@users.noreply.github.com>
2024-12-17 17:25:21 +0900

    WIP: refactoring sv_detector to report consensus

--- src/DAJIN2/core/preprocess/sv_detector.py
+++ src/DAJIN2/core/preprocess/sv_detector.py
@@ -168,20 +168,44 @@ def extract_sv_features(sv_tags: list[str], sv_type: str, sv_index: list[int]) -
 ###############################################################################
 
 
-def group_cssplits(cssplits: list[str]) -> list[str]:
-    cssplits_grouped = []
+def _group_cssplits(cssplits: list[str]) -> Iterator[str]:
     current_group = [cssplits[0]]
+    current_start_index = 0
 
     for i in range(1, len(cssplits)):
         # 現在の要素のprefixと前の要素のprefixが同じ場合
         if cssplits[i][0] == cssplits[i - 1][0]:
             current_group.append(cssplits[i])
         else:
-            cssplits_grouped.append(",".join(current_group))  # グループを結果に追加
+            yield (current_start_index, ",".join(current_group))  # グループを結果に追加
             current_group = [cssplits[i]]  # 新しいグループを開始
+            current_start_index = i
 
-    cssplits_grouped.append(",".join(current_group))  # 最後のグループを追加
-    return cssplits_grouped
+    yield (i, ",".join(current_group))  # 最後のグループを追加
+
+
+def _filter_valid_cssplits(cssplits, index_converter):
+    """有効な cssplits をフィルタリングする"""
+    # TODO: Deletion, Insertion, Inversionで適切な処理を変更する
+    return [
+        (i, cssplit)
+        for i, cssplit in cssplits
+        if cssplit.startswith("+") and cssplit.count("+") > 10 and i in index_converter
+    ]
+
+
+def _convert_insertion_cstag_to_cssplit(cs_tag: str) -> str:
+    # TODO: Deletion, Insertion, Inversionで適切な処理を変更する
+    pattern = re.compile(r"([\+\-=])([a-zA-Z]+)(.*)")
+    match = pattern.match(cs_tag)
+    if not match:
+        return cs_tag  # マッチしない場合はそのまま返す
+    prefix, letters, suffix = match.groups()  # グループを取得
+    # 文字ごとにprefixを付けて'|'で結合
+    transformed = "|".join(f"{prefix}{char}" for char in letters)
+
+    # 最後の部分を追加
+    return f"{transformed}{suffix}".upper()
 
 
 # すべての配列をシードとアラインメントし、その結果を保存
@@ -214,40 +238,80 @@ def _build_consensus(aligned_seqs):
     return "".join(consensus).replace("-", "")  # 最終的にギャップを取り除く
 
 
-def _group_cssplits(cssplits: list[str]) -> Iterator[str]:
-    current_group = [cssplits[0]]
-    current_start_index = 0
-
-    for i in range(1, len(cssplits)):
-        # 現在の要素のprefixと前の要素のprefixが同じ場合
-        if cssplits[i][0] == cssplits[i - 1][0]:
-            current_group.append(cssplits[i])
-        else:
-            yield (current_start_index, ",".join(current_group))  # グループを結果に追加
-            current_group = [cssplits[i]]  # 新しいグループを開始
-            current_start_index = i
-
-    yield (i, ",".join(current_group))  # 最後のグループを追加
-
-
-def convert_insertion_cstag_to_cssplit(input_string):
-    pattern = re.compile(r"([\+\-=])([a-zA-Z]+)(.*)")
-    match = pattern.match(input_string)
-    if not match:
-        return input_string  # マッチしない場合はそのまま返す
-    prefix, letters, suffix = match.groups()  # グループを取得
-    # 文字ごとにprefixを付けて'|'で結合
-    transformed = "|".join(f"{prefix}{char}" for char in letters)
-
-    # 最後の部分を追加
-    return f"{transformed}{suffix}".upper()
-
-
-def call_consensus(sequences: list[str]) -> str:
+def _call_consensus(sequences: list[str]) -> str:
     # 最初に最も長いシード配列を選定
     seed = max(sequences, key=len)
     aligned_sequences = _align_sequences(seed, sequences)
     return _build_consensus(aligned_sequences)
+
+
+def _generate_consensus_for_label(cssplits_subset, index_converter, sv_type: str) -> dict[int, str]:
+    """cssplits_subset からインデックスごとのコンセンサスを生成する"""
+    csv_group = defaultdict(list)
+
+    for cssplits in cssplits_subset:
+        for i, cssplit in _group_cssplits(cssplits):
+            if_valid = _filter_valid_cssplits([(i, cssplit)], index_converter)  # TODO
+            for i, valid_cssplit in if_valid:
+                cs_tags = convert_cssplits_to_cstag([valid_cssplit])
+                csv_group[index_converter[i]].append(cs_tags)
+
+    index_consensus = {}
+    for key, value in csv_group.items():
+        if sv_type == "insertion":
+            ins_seqs, end_seqs = zip(*[cstag.split(tag) for tag in value])
+            consensus_ins = _call_consensus(ins_seqs) + "|" + _call_consensus(end_seqs)
+            index_consensus[key] = _convert_insertion_cstag_to_cssplit(consensus_ins)  # TODO
+        else:
+            index_consensus[key] = _call_consensus(value)
+    return index_consensus
+
+
+def _replace_cssplits_with_consensus(cssplits_subset, index_consensus, index_converter):
+    """cssplits_subset の中でコンセンサスに置き換える"""
+    for cssplits in cssplits_subset:
+        for i, cssplit in _group_cssplits(cssplits):
+            if_valid = _filter_valid_cssplits([(i, cssplit)], index_converter)
+            for i, _ in if_valid:
+                cssplits[i] = index_consensus[index_converter[i]]
+
+
+###############################################################################
+# cstagとfastaをラベルごとに生成する
+###############################################################################
+
+
+def generate_cstag_and_fasta(
+    path_midsv: Path, labels: list[int], index_converter: dict[int, int], sv_type: str
+) -> tuple[dict[int, str], dict[int, str]]:
+    """cstag と fasta をラベルごとに生成する"""
+
+    midsv_sample = io.read_jsonl(path_midsv)
+    cssplits_iter: Iterator[list[list[str]]] = (m["CSSPLIT"].split(",") for m in midsv_sample)
+
+    cssplits_by_label = defaultdict(list)
+    for label, cssplits in zip(labels, cssplits_iter):
+        cssplits_by_label[label].append(cssplits)
+
+    cstag_by_label = {}
+    fasta_by_label = {}
+
+    for label, cssplits in cssplits_by_label.items():
+        cssplits_subset = cssplits[:100]
+
+        # コンセンサスの生成
+        index_consensus = _generate_consensus_for_label(cssplits_subset, index_converter, sv_type)
+        _replace_cssplits_with_consensus(cssplits_subset, index_consensus, index_converter)
+
+        # コンセンサス cstag と fasta の生成
+        cstag_subset = [convert_cssplits_to_cstag(tag) for tag in cssplits_subset]
+        positions = [1] * len(cstag_subset)
+        cstag_consensus = cstag.consensus(cstag_subset, positions)
+
+        cstag_by_label[label] = cstag_consensus
+        fasta_by_label[label] = cstag.to_sequence(cstag_consensus)
+
+    return cstag_by_label, fasta_by_label
 
 
 ###############################################################################
@@ -259,6 +323,9 @@ def detect_sv_alleles(TEMPDIR: Path, SAMPLE_NAME: str, CONTROL_NAME: str, FASTA_
     path_midsv_sample = Path(TEMPDIR, SAMPLE_NAME, "midsv", "control", f"{SAMPLE_NAME}.jsonl")
     midsv_sample = io.read_jsonl(path_midsv_sample)
     sv_tags_sample = [convert_sv_tag(m["CSSPLIT"], sv_type) for m in midsv_sample]
+    MUTATION_LOCI = io.load_pickle(
+        Path(TEMPDIR, SAMPLE_NAME, "mutation_loci", "control", "mutation_loci.pickle")
+    )  # TODO: MUTATION_LOCIをコンセンサスに反映させる
 
     path_midsv_control = Path(TEMPDIR, CONTROL_NAME, "midsv", "control", f"{CONTROL_NAME}.jsonl")
     midsv_control = io.read_jsonl(path_midsv_control)
@@ -279,55 +346,11 @@ def detect_sv_alleles(TEMPDIR: Path, SAMPLE_NAME: str, CONTROL_NAME: str, FASTA_
 
     labels = optimize_labels(X, coverage_sample, coverage_control)
 
-    midsv_sample = io.read_jsonl(path_midsv_sample)
-    cssplits_iter: Iterator[list[list[str]]] = (m["CSSPLIT"].split(",") for m in midsv_sample)
-
-    cssplits_by_label = defaultdict(list)
-    for label, cssplits in zip(labels, cssplits_iter):
-        cssplits_by_label[label].append(cssplits)
-
     #######################################################
     # Generate cstag consensus
     #######################################################
 
-    cstag_by_label = {}
-    fasta_by_label = {}
-    for label, cssplits in cssplits_by_label.items():
-        cssplits_subset = cssplits[:100]
-        csv_group = defaultdict(list)
-        for cssplits in cssplits_subset:
-            cssplits_grouped = _group_cssplits(cssplits)
-            for i, cssplit in cssplits_grouped:
-                if cssplit.startswith("+") and cssplit.count("+") > 10 and i in index_converter:
-                    cs_tags = convert_cssplits_to_cstag([cssplit])
-                    csv_group[index_converter[i]].append(cs_tags)
-
-        index_consensus = {}
-        for key, value in csv_group.items():
-            ins_seqs, end_seqs = zip(*[cstag.split(tag) for tag in value])
-            consensus_ins = call_consensus(ins_seqs) + "|" + call_consensus(end_seqs)
-            index_consensus |= {key: convert_insertion_cstag_to_cssplit(consensus_ins)}
-
-        for cssplits in cssplits_subset:
-            for i, cssplit in _group_cssplits(cssplits):
-                if cssplit.startswith("+") and cssplit.count("+") > 10 and i in index_converter:
-                    cssplits[i] = index_consensus[index_converter[i]]
-
-        cstag_subset = [convert_cssplits_to_cstag(tag) for tag in cssplits_subset]
-        positions = [1] * len(cstag_subset)
-        cstag_consensus = cstag.consensus(cstag_subset, positions)
-        cstag_by_label[label] = cstag_consensus
-        fasta_by_label[label] = cstag.to_sequence(cstag_consensus)
-
-    # cstag_by_label = {}
-    # fasta_by_label = {}
-    # for label, cssplits in cssplits_by_label.items():
-    #     cssplits_subset = cssplits[:100]
-    #     cstag_subset = [convert_cssplits_to_cstag(tag) for tag in cssplits_subset]
-    #     positions = [1] * len(cstag_subset)
-    #     cstag_consensus = cstag.consensus(cstag_subset, positions)
-    #     cstag_by_label[label] = cstag_consensus
-    #     fasta_by_label[label] = cstag.to_sequence(cstag_consensus)
+    cstag_by_label, fasta_by_label = generate_cstag_and_fasta(path_midsv_sample, labels, index_converter, sv_type)
 
     #######################################################
     # Remove similar alleles to user's alleles, or clustered alleles
